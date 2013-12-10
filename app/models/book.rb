846dd66ec6bd1cde9cffeb2b67e59307ef027241
B Alter <barnaby.alter@gmail.com>
2013-12-10 13:08:19 -0500

    Added page and relatinos

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,8 @@
 class Book < ActiveFedora::Base
+  
   has_metadata 'descMetadata', type: BookMetadata
+
+  has_many :pages, :property => :is_part_of
 
   has_attributes :title, datastream: 'descMetadata', multiple: false
   has_attributes :author, datastream: 'descMetadata', multiple: false
--- app/models/datastreams/book_metadata.rb
+++ app/models/datastreams/book_metadata.rb
@@ -2,8 +2,8 @@ class BookMetadata < ActiveFedora::OmDatastream
 
   set_terminology do |t|
     t.root(path: "fields")
-    t.title(index_as: :stored_searchable)
-    t.author(index_as: :stored_searchable)
+    t.title index_as: :stored_searchable
+    t.author index_as: :stored_searchable
   end
 
   def self.xml_template
