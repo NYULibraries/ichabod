58d5a4d8e82b0a2d2daa73ab76b3076624b5d259
B Alter <barnaby.alter@gmail.com>
2013-12-18 10:36:31 -0500

    Removed environment vars from cap deploy, moved to jenkins config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,45 @@
-require 'nyulibraries/deploy/capistrano'
-set :app_title, "hydra-nyu"
-set :recipient, "web.services@library.nyu.edu"
+# Multistage
+require 'capistrano/ext/multistage'
+# Load bundler-capistrano gem
+require "bundler/capistrano"
+# Load rvm-capistrano gem
+require "rvm/capistrano"
+# Multistage
+require 'capistrano/ext/multistage'
+
+# Environments
+set :stages, ["staging"]
+set :default_stage, "staging"
+
+set :ssh_options, {:forward_agent => true}
+set(:app_title) { "hydra-nyu" }
+set(:application) { "#{app_title}_repos" }
+
+# RVM  vars
+set :rvm_ruby_string, "2.0.0-p353"
+set :rvm_type, :user
+
+# Git vars
+set :repository, "git@github.com:NYULibraries/hydra-nyu.git" 
+set :scm, :git
+set :deploy_via, :checkout
+set(:branch, 'master') unless exists?(:branch)
+set :git_enable_submodules, 1
+
+set :keep_releases, 5
+set :use_sudo, false
+
+set :app_path, ENV["APP_PATH"]
+set(:deploy_to) { "#{app_path}#{application}" }
+
+namespace :deploy do
+  task :create_symlink do
+    run "rm -rf #{app_path}#{app_title} && ln -s #{current_path} #{app_path}#{app_title}"
+  end
+  task :create_current_path_symlink do
+    run "rm -rf #{current_path} && ln -s #{current_release} #{current_path}"
+  end
+end
+
+before "deploy", "rvm:install_ruby"
+after "deploy", "deploy:create_symlink", "deploy:create_current_path_symlink", "deploy:cleanup"
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,3 +1,2 @@
 set :rails_env, "staging"
 set :branch, "master"
-server "webdev3.library.nyu.edu", :app, :web, :db, :primary => true
