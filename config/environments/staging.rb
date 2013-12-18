7b81a9fb1041718e9015e9d572dcc969af87b488
B Alter <barnaby.alter@gmail.com>
2013-12-18 13:15:59 -0500

    Fixing staging environment for fedora and solr and making assets work

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,4 +1,5 @@
 HydraDemo::Application.configure do
+  config.action_controller.relative_url_root = '/hydra-nyu'
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
