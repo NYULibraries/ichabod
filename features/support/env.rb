7533ef7ffb10801bdbd73e8625e23ff5a8e3134d
B Alter <barnaby.alter@gmail.com>
2014-05-06 12:01:22 -0400

    Created a cucumber environment

--- features/support/env.rb
+++ features/support/env.rb
@@ -6,14 +6,17 @@
 
 require 'cucumber/rails'
 
+ENV['RAILS_ENV'] = 'cucumber'
+Rails.env = 'cucumber'
+
 # Capybara defaults to CSS3 selectors rather than XPath.
 # If you'd prefer to use XPath, just uncomment this line and adjust any
 # selectors in your step definitions to use the XPath syntax.
 # Capybara.default_selector = :xpath
 
 # By default, any exception happening in your Rails application will bubble up
-# to Cucumber so that your scenario will fail. This is a different from how 
-# your application behaves in the production environment, where an error page will 
+# to Cucumber so that your scenario will fail. This is a different from how
+# your application behaves in the production environment, where an error page will
 # be rendered instead.
 #
 # Sometimes we want to override this default behaviour and allow Rails to rescue
@@ -55,4 +58,3 @@ end
 # The :transaction strategy is faster, but might give you threading problems.
 # See https://github.com/cucumber/cucumber-rails/blob/master/features/choose_javascript_database_strategy.feature
 Cucumber::Rails::Database.javascript_strategy = :truncation
-
