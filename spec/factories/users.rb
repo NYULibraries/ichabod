5f10ef68911adb7cb3a32c33b8ff7f670d78389e
B Alter <barnaby.alter@gmail.com>
2014-05-08 12:49:16 -0400

    Simplecov unviersal config and some more specs

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,6 +3,7 @@ FactoryGirl.define do
   factory User do
     sequence(:username) { |n| "user#{n}" }
     sequence(:email) { |n| "#{username}@example.com" }
+    user_attributes {{ :some_field => "test" }}
   end
 
 end
