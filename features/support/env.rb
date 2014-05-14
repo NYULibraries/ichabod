4e925eaf34952ba35a6beaeb2703b71c430beb0c
B Alter <barnaby.alter@gmail.com>
2014-05-14 18:33:09 -0400

    Mocking login with vcr

--- features/support/env.rb
+++ features/support/env.rb
@@ -55,7 +55,6 @@ end
 #     DatabaseCleaner.strategy = :transaction
 #   end
 #
-
 # Possible values are :truncation and :transaction
 # The :transaction strategy is faster, but might give you threading problems.
 # See https://github.com/cucumber/cucumber-rails/blob/master/features/choose_javascript_database_strategy.feature
--- features/support/vcr.rb
+++ features/support/vcr.rb
@@ -1,7 +1,6 @@
 require 'vcr'
 
 VCR.configure do |c|
-  c.default_cassette_options = { :record => :new_episodes }
   c.hook_into :webmock
   c.cassette_library_dir     = 'features/cassettes'
   c.ignore_localhost = true
