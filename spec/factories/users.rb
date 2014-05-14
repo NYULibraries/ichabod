b525c03544299a835b53671f5e9c695f99dfd40a
B Alter <barnaby.alter@gmail.com>
2014-05-14 18:33:09 -0400

    Mocking login with vcr

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,8 +2,9 @@ FactoryGirl.define do
 
   factory User do
     sequence(:username) { |n| "user#{n}" }
-    sequence(:email) { |n| "#{username}@example.com" }
+    sequence(:email) { |n| "user#{n}@nyu.edu" }
     user_attributes {{ :some_field => "test" }}
   end
+
 
 end
