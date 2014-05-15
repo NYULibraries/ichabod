7cddc90e0badae57a3f4cfd346db6215e185e698
B Alter <barnaby.alter@gmail.com>
2014-05-15 15:31:25 -0400

    Also ignore selenium specific requests

--- features/support/vcr.rb
+++ features/support/vcr.rb
@@ -1,11 +1,12 @@
 require 'vcr'
 
 VCR.configure do |c|
-  # Capybara with poltergeist js driver uses this __identify__ path
+  # Capybara with poltergeist js driver uses this /__identify__ path
   # which we want to always ignore in VCR
+  # Selenium uses the /hub/session path
   # See: https://github.com/vcr/vcr/issues/229
   c.ignore_request do |request|
-    URI(request.uri).path == "/__identify__"
+    URI(request.uri).path == "/__identify__" || URI(request.uri).path =~ /\/hub\/session/
   end
   c.default_cassette_options = { :allow_playback_repeats => true }
   c.hook_into :webmock
