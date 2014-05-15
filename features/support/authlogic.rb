6331925e4cf6c4f449258e9e8b5f44f5d79d833e
Scot Dalton <scotdalton@gmail.com>
2014-05-14 22:35:48 -0400

    Uncomment out pds handle login method in the authlogic file

--- features/support/authlogic.rb
+++ features/support/authlogic.rb
@@ -8,10 +8,19 @@
 # end
 
 # 2) Open up UserSession and set pds_handle to a handle which we have previously recorded a VHS for
-# UserSession.class_eval do
-#   def pds_handle
-#     # Hardcoded now but need to figure out how make it read from an env var
-#     ENV["PDS_HANDLE"] || super
-#     # "GIS_Cataloger"
-#   end
-# end
+UserSession.class_eval do
+
+  # Override pds_handle to use ENV['PDS_HANDLE'] if it's available
+  def pds_handle
+    # Set PDS handle in an environment variable if you want to
+    # override super, e.g. ENV['PDS_HANDLE'] = 'GIS_Cataloger'
+    pds_handle_lambda.call || super
+  end
+
+  # Read PDS handle from an evironment variable, PDS_HANDLE
+  # It will return nil if ENV['PDS_HANDLE'] is not set
+  def pds_handle_lambda
+    -> { ENV['PDS_HANDLE'] }
+  end
+  private :pds_handle_lambda
+end
