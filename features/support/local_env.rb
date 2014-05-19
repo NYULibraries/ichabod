ea57e02d8b32c513365667ff991091d30103f591
B Alter <barnaby.alter@gmail.com>
2014-05-19 11:55:51 -0400

    Removing lines to try to make this work with travis, that needs to be tackled separately

--- features/support/local_env.rb
+++ features/support/local_env.rb
@@ -8,7 +8,7 @@ end
 
 require 'capybara/poltergeist'
 
-if ENV['IN_BROWSER']
+if ENV['IN_BROWSER'] || ENV['TRAVIS']
   # On demand: non-headless tests via Selenium/WebDriver
   # To run the scenarios in browser (default: Firefox), use the following command line:
   # IN_BROWSER=true bundle exec cucumber
