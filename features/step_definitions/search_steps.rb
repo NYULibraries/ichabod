b5ec3bc58227ca770ff227c56a8bfa33f9158c15
ekate <ekate@nyu.edu>
2014-05-19 23:06:19 -0400

    Modified rspec cassette use

--- features/step_definitions/search_steps.rb
+++ features/step_definitions/search_steps.rb
@@ -26,3 +26,21 @@ And(/^I limit the search by a facet "(\w+.*?\s\w+.*?)"$/) do |facet|
     click_link("#{facet}")
   end
 end
+
+  Given(/^I Limit search to "(.*?)" in "(.*?)"$/) do |value,facet|
+    visit root_path
+    within(:css, '#facets') do
+      click_link("#{facet}")
+      click_link("#{value}")
+    end
+  end
+
+Given(/^I search for "(.*?)"$/) do |value|
+   step %{I search on the phrase "#{value}"}
+end
+
+Then(/^I get dataset with title "(.*?)"$/) do |title|
+   node=page.find(:xpath, '//div[@id="documents"]')
+   node.should have_link(title)
+end
+
