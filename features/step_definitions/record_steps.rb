ed87e06271b9e1b7f5d94a117586d09a47f8bcb1
B Alter <barnaby.alter@gmail.com>
2014-05-21 16:40:01 -0400

    Added cucumber tests for metadata remediation

--- features/step_definitions/record_steps.rb
+++ features/step_definitions/record_steps.rb
@@ -15,3 +15,31 @@ end
 Then(/^I should see the message "(.+?)"$/) do |message|
   expect(page.find(:css, "div.alert.alert-info")).to have_content message
 end
+
+Given(/^the record "(.*?)" exists$/) do |title|
+  @record = FactoryGirl.create(format_factory(title))
+end
+
+Given(/^I am on the "Edit Item" form for "(.*?)"$/) do |title|
+  visit edit_nyucore_path(@record)
+end
+
+When(/^I delete the record$/) do
+  visit nyucores_path
+  accept_confirms
+  page.find(:xpath, "//a[@href='#{nyucore_path(@record)}'][@data-method='delete']").click
+end
+
+When(/^I click on "(.+?)"$/) do |link_text|
+  click_on(link_text)
+end
+
+Given(/^I click on all the "(.*?)" buttons$/) do |button_text|
+  page.all(:xpath, "//a[text()='#{button_text}']").each do |button|
+    button.click
+  end
+end
+
+Then(/^I should see the fields:$/) do |table|
+  pending
+end
---
+++ features/support/helpers/record.rb
@@ -0,0 +1,13 @@
+module IchabodFeatures
+  module Record
+
+    def format_factory(factory_title)
+      factory_title.gsub(/ /,"_").downcase.to_sym
+    end
+
+    def accept_confirms
+      page.evaluate_script('window.confirm = function() { return true; }')
+    end
+    
+  end
+end
