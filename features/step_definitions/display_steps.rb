6870a7a4a4b2419624007c89528d3d4b1151e3de
Corey Harper <corey.harper@gmail.com>
2014-06-02 17:47:15 -0400

    Add helpers/selectors.rb for display cukes; checkout logged_in_users.yml; re-record cassettes

--- features/step_definitions/display_steps.rb
+++ features/step_definitions/display_steps.rb
@@ -1,22 +1,19 @@
 
 
 Then(/^I should get a dataset with the link "(.*?)"$/) do |download|
-   node = page.find(:xpath, '//div[@id="content"]')
-   node.should have_link(download)
+  documents_container.should have_link(download)
 end
 
 Then(/^I should get "(.*?)" field in all results$/) do |field|
-  node = page.find(:xpath, '//div[@id="content"]')
-  expect(node.all('dt', :text => field).count).to be == 10
+  expect(documents_container.all('dt', :text => field).count).to be == 10
 end
 
 And(/^I navigate to details display of the first result$/) do
-  within(page.find(:xpath, '//div[@id="content"]')) do
+  within(documents_container) do
     find(:xpath, "//a[@data-counter='1']").click
   end
 end
 
 Then(/^I should get "(.*?)" field in the details display$/) do |field|
-  node = page.find(:xpath, '//div[@id="content"]')
-  expect(node.all('dt', :text => field).count).to be == 1
+  expect(document_container.all('dt', :text => field).count).to be == 1
 end
