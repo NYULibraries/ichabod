c7e9ca2d53324b4d8c2161784baaf6be557f9f39
Corey Harper <corey.harper@gmail.com>
2014-06-02 17:47:15 -0400

    Add helpers/selectors.rb for display cukes; checkout logged_in_users.yml; re-record cassettes

---
+++ features/support/helpers/selectors.rb
@@ -0,0 +1,15 @@
+module IchabodFeatures
+  module Selectors
+    def documents_container
+      page.find("#content")
+    end
+  end
+end
+
+module IchabodFeatures
+  module Selectors
+    def document_container
+      page.find("#content")
+    end
+  end
+end
