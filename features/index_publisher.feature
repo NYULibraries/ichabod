5f018afb3f359934869798d2bb2d86b4b269eb68
B Alter <barnaby.alter@gmail.com>
2014-06-04 13:26:23 -0400

    DRYed up cuke steps, reworded the facet language; :gem: downgrade for webmock

--- features/index_publisher.feature
+++ features/index_publisher.feature
@@ -8,4 +8,4 @@ Feature: Index publisher
     Given I am on the default search page
     When I search on the phrase "ESRI"
     Then I should see search results
-    And I should see a Geospatial Data facet under Format
+    And I should see a "Geospatial Data" facet under the "Format" category
--- features/support/helpers/selectors.rb
+++ features/support/helpers/selectors.rb
@@ -1,15 +1,17 @@
 module IchabodFeatures
   module Selectors
-    def documents_container
-      page.find("#content")
-    end
-  end
-end
 
-module IchabodFeatures
-  module Selectors
-    def document_container
-      page.find("#content")
+    def documents_list_container
+      page.find("#documents")
     end
+
+    def documents_list
+      documents_list_container.all(".document")
+    end
+
+    def document_container
+      page.find("#document")
+    end
+
   end
 end
