0fcb5e50b1362177ec942ca67b4fee42dfd2c931
Corey Harper <corey.harper@gmail.com>
2014-06-07 14:53:48 -0400

    Cucumber steps for addinfo link

--- features/step_definitions/display_steps.rb
+++ features/step_definitions/display_steps.rb
@@ -15,3 +15,16 @@ end
 Then(/^I should get "(.*?)" field in the details display$/) do |field|
   expect(document_container.all('dt', :text => field)).to have_exactly(1).item
 end
+
+Then(/^I should see the link "(.*?)" in the "(.*?)" field$/) do |link, field|
+  # Two possible approaches here. Find that there is one "Additional Information" field
+  # And that there is one field with a specific link...
+  expect(document_container.all('dt', :text => field)).to have_exactly(1).item
+  expect(document_container).to have_link(link)
+  # ...or try to find the relevant dt/dd pair based on its field & confirm its link.
+  # I think I prefer the later pattern
+  within(document_container) do
+    expect(find(:xpath, "//dt[text()='#{field}']/following-sibling::dd")).to have_link(link)
+
+  end
+end
