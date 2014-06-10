b2c862adb95f6a8072747d3938d27acd49242f48
B Alter <barnaby.alter@gmail.com>
2014-06-10 16:09:10 -0400

    Reconfigure deploy to stop and start jetty around deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,12 +22,12 @@ end
 
 namespace :ingest do
   task :load_xml_data do
-    run "bundle exec rake ichabod:load[./ingest/sdr.xml,sdr]"
-    run "bundle exec rake ichabod:load[./ingest/stern.xml,fda]"
+    run "bundle exec rake ichabod:load['./ingest/sdr.xml','sdr']"
+    run "bundle exec rake ichabod:load['./ingest/stern.xml','fda']"
   end
   task :clean_xml_data do
-    run "bundle exec rake ichabod:delete[./ingest/sdr.xml,sdr]"
-    run "bundle exec rake ichabod:delete[./ingest/stern.xml,fda]"
+    run "bundle exec rake ichabod:delete['./ingest/sdr.xml','sdr']"
+    run "bundle exec rake ichabod:delete['./ingest/stern.xml','fda']"
   end
 end
 
@@ -37,6 +37,18 @@ namespace :cache do
   end
 end
 
-after "deploy", "deploy:create_jetty_symlink"
+namespace :jetty do
+  desc "Shutdown previous version of jetty on server"
+  task :stop do
+    run "bundle exec rake jetty:stop"
+  end
+  desc "Startup new jetty for current release"
+  task :start do
+    run "bundle exec rake jetty:start"
+  end
+end
+
+before "deploy", "jetty:stop"
+after "deploy", "deploy:create_jetty_symlink", "jetty:start"
 
 # after "deploy", "deploy:create_symlink", "deploy:create_current_path_symlink", "deploy:create_env_symlink"
