a93716deb7eb8779d852c268094a54b029e5b999
B Alter <barnaby.alter@gmail.com>
2014-07-25 14:14:13 -0400

    Removed set_item function from nyucore controller and changed blank_to_nil_params to remove blank values with array params as well

--- app/controllers/nyucores_controller.rb
+++ app/controllers/nyucores_controller.rb
@@ -1,9 +1,8 @@
 class NyucoresController < ApplicationController
   respond_to :html, :json
-  before_action :set_item, only: [:edit, :update]
   # Convert blank values to nil in params when creating and updating
   # in order to not save empty array values when field is not nil but is an empty string (i.e. "")
-  before_filter :blank_to_nil_params, :only => [:create, :update]
+  before_action :blank_to_nil_params, :only => [:create, :update]
 
   def index
     @items = Nyucore.all
@@ -24,6 +23,7 @@ class NyucoresController < ApplicationController
   end
 
   def edit
+    @item = Nyucore.find(params[:id])
     respond_with(@item)
   end
 
@@ -35,6 +35,7 @@ class NyucoresController < ApplicationController
   end
 
   def update
+    @item = Nyucore.find(params[:id])
     # Not authorizing resources for now
     # authorize! :update, params[:id]
 
@@ -52,10 +53,6 @@ class NyucoresController < ApplicationController
 
   private
 
-  def set_item
-    @item = Nyucore.find(params[:id])
-  end
-
   # Whitelist attrs
   def item_params
     params.require(:nyucore).permit(:title, :creator, :publisher, :identifier, :type, :available => [], :description=> [], :edition=> [], :series=> [], :version=> [], :date=> [], :format=> [], :language=> [], :relation=> [], :rights=> [], :subject=> [], :citation=> [])
@@ -63,7 +60,8 @@ class NyucoresController < ApplicationController
 
   # Convert blank values to nil values in params
   # in order to not save empty array values when field is not nil but is an empty string (i.e. "")
+  # Added the reject statement to get rid of blank values for array params
   def blank_to_nil_params
-    params[:nyucore].merge!(params[:nyucore]){|k, v| v.blank? ? nil : v}
+    params[:nyucore].merge!(params[:nyucore]){|k, v| v.blank? ? nil : v.reject{|c| c.empty? }}
   end
 end
