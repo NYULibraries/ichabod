8a257120e0ade7dca9366ae6a206e1235979734b
eshadatta <ed64@nyu.edu>
2014-07-25 13:59:44 -0400

    unfortunately incomplete user story

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,19 @@
+
 module ApplicationHelper
 
   # Taken from RockHall code:
   # https://github.com/awead/catalog/blob/1d4be363fc2a6f490b4f3343219181c581e130cc/app/helpers/marc_helper.rb
   def render_external_link args, results = Array.new
     begin
+    
       value = args[:document][args[:field]]
+      document = args[:document]
+      field = args[:field]
+
+      decorator = SolrDocumentUrlFieldsDecorator.new(document, field, blacklight_config)
+       url = decorator.test
+
+    
       if value.length > 1
         value.each_index do |index|
           text      = args[:document][blacklight_config.show_fields[args[:field].to_s][:text]][index]
@@ -13,10 +22,14 @@ module ApplicationHelper
           results << link_to(link_text, url, { :target => "_blank" })
         end
       else
-        text      = args[:document].get(blacklight_config.show_fields[args[:field].to_s][:text])
-        url       = args[:document].get(args[:field])
-        metadata_type = args[:document]["desc_metadata__type_tesim"] 
+
+        text      = document.get(blacklight_config.show_fields[field.to_s][:text])
+   
+        url       = document.get(field)
+       
+        metadata_type = document["desc_metadata__type_tesim"] 
         text = "Download" if metadata_type[0] == "Geospatial Data"
+
         link_text = text.nil? ? url : text
         results << link_to(link_text, url, { :target => "_blank" })
       end
