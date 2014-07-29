04fb67e76c4faab1ce84fbbd838b662443bd4d24
Scot Dalton <scotdalton@gmail.com>
2014-07-29 14:59:06 -0400

    Refactor nested UrlPresent::Url class for maintainability

--- spec/presenters/url_presenter/url_spec.rb
+++ spec/presenters/url_presenter/url_spec.rb
@@ -1,25 +1,23 @@
 require 'spec_helper'
-class UrlPresenter < CatalogPresenter
-  describe Url do
-    let(:value) { 'value' }
-    let(:text) { 'text' }
-    subject(:url) { Url.new(value, text) }
-    it { should be_a Url }
-    describe '#value' do
-      subject { url.value }
+describe UrlPresenter::Url do
+  let(:value) { 'value' }
+  let(:text) { 'text' }
+  subject(:url) { UrlPresenter::Url.new(value, text) }
+  it { should be_a UrlPresenter::Url }
+  describe '#value' do
+    subject { url.value }
+    it { should eq value }
+  end
+  describe '#text' do
+    subject { url.text }
+    it { should eq text }
+    context 'when the text parameter is nil' do
+      let(:text) { nil }
       it { should eq value }
     end
-    describe '#text' do
-      subject { url.text }
-      it { should eq text }
-      context 'when the text parameter is nil' do
-        let(:text) { nil }
-        it { should eq value }
-      end
-      context 'when initialized without a text parameter' do
-        let(:url) { Url.new(value) }
-        it { should eq value }
-      end
+    context 'when initialized without a text parameter' do
+      let(:url) { UrlPresenter::Url.new(value) }
+      it { should eq value }
     end
   end
 end
