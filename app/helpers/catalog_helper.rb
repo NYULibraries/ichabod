4a0f54a56cd9abb7e697720cfd5cb23308d3d59b
Scot Dalton <scotdalton@gmail.com>
2014-07-29 15:00:04 -0400

    Mimic Blacklight helpers for now

--- app/helpers/catalog_helper.rb
+++ app/helpers/catalog_helper.rb
@@ -1,4 +1,6 @@
 module CatalogHelper
+  include Blacklight::CatalogHelperBehavior
+
   # Taken from RockHall code:
   # https://github.com/awead/catalog/blob/1d4be363fc2a6f490b4f3343219181c581e130cc/app/helpers/marc_helper.rb
   def render_external_link(args)
