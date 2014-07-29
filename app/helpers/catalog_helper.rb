9c34a00ec308d3385bbae695734bff440c50d8cd
Scot Dalton <scotdalton@gmail.com>
2014-07-29 15:57:40 -0400

    Change the name of the links rendering helper method to render_external_links since links are multivalue elements

--- app/helpers/catalog_helper.rb
+++ app/helpers/catalog_helper.rb
@@ -3,7 +3,7 @@ module CatalogHelper
 
   # Taken from RockHall code:
   # https://github.com/awead/catalog/blob/1d4be363fc2a6f490b4f3343219181c581e130cc/app/helpers/marc_helper.rb
-  def render_external_link(args)
+  def render_external_links(args)
     document = args[:document]
     field_name = args[:field]
     url_presenter = UrlPresenter.new(document, field_name)
