60d7cc0ecc79c4c5a893eb07ac920f59ed8c3af1
B Alter <barnaby.alter@gmail.com>
2014-08-01 10:25:02 -0400

    Made collections a little slicker with forwardable and some config lambdas, too slick probably; tested

--- app/models/nyucore/collections.rb
+++ app/models/nyucore/collections.rb
@@ -2,7 +2,10 @@ class Nyucore < ActiveFedora::Base
   class Collections
     include Enumerable
 
-    COLLECTION_MAP = { "ESRI" => { publisher: ["ESRI"] }, "Spatial Data Repository" => { type: ["Geospatial Data"] } }
+    COLLECTION_MAP = {
+      "ESRI" => ->(nyucore){ nyucore.publisher.include? "ESRI" },
+      "Spatial Data Repository" => ->(nyucore){ nyucore.type.include? "Geospatial Data" }
+    }
 
     attr_reader :nyucore
 
@@ -13,31 +16,18 @@ class Nyucore < ActiveFedora::Base
       @nyucore = nyucore
     end
 
-    def each(&block)
-      collections.each(&block)
-    end
+    # Implement each as collections
+    extend Forwardable
+    def_delegator :collections, :each
 
   private
 
     ##
     # Get array of collections from mapping
     def collections
-      @collections = map_to_collection(:publisher)
-      @collections |= map_to_collection(:type)
-      @collections.reject {|c| c.nil?}
-    end
-
-    ##
-    # Map values in from_field to the collection value held in the constant
-    # Ex:
-    # Given an object <Nyucore type: ["Geospatial Data","Blahblah"]>
-    # map_to_collection(:type) # ["Spatial Data Repository"]
-    def map_to_collection(from_field)
-      # Return a new hash from the collection map
-      # where this object has a value in from_field that matches
-      # one of the values in COLLECTION_MAP
-      map_to_collection = COLLECTION_MAP.select {|c| COLLECTION_MAP[c][from_field].present? && COLLECTION_MAP[c][from_field].any? {|ff| nyucore.send(from_field).include? ff }}
-      map_to_collection.keys
+      @collections ||= COLLECTION_MAP.select do |key,  matcher|
+        matcher.call(nyucore)
+      end.keys
     end
   end
 end
