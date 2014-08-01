60d231c0ac995fbf3e91ff4faa8ba97d46c19d80
B Alter <barnaby.alter@gmail.com>
2014-08-01 10:48:05 -0400

    This time with tests passing

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,12 +3,6 @@ class Ability
 
   # Define any customized permissions here.
   def custom_permissions
-    # Limits deleting objects to a the admin user
-    #
-    if current_user.admin?
-      can [:destroy], ActiveFedora::Base
-    end
-
     # Aliasing create/read/update/delete actions
     alias_action :create, :read, :update, :destroy, :to => :crud
     # Limits creating new objects to a specific group
