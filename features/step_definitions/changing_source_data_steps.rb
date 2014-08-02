f4f4b767bf45dcbe475fcc3846a23b817b0333f6
Corey Harper <corey.harper@gmail.com>
2014-08-02 11:28:40 -0400

    Collapsed to single change/revert step. Rename fixtures. Add data_loader helper.

--- features/step_definitions/changing_source_data_steps.rb
+++ features/step_definitions/changing_source_data_steps.rb
@@ -1,11 +1,13 @@
-Given (/^I change the "(.*?)" source data in the "(.*?)" field to "(.*?)" for the record identified by "(.*?)"$/) do |source, field, data, identifier|
-  if data == "New York City Dept of City Planning" then 
-    @file = "spec/fixtures/new_battalions.xml" 
-  elsif data == "New York City Department of City Planning" then  
-    @file = "spec/fixtures/orig_battalions.xml" 
+require 'data_loader'
+
+Given (/^I "(change|revert)?" the "(.*?)" source data in the "(.*?)" field to "(.*?)" for the record identified by "(.*?)"$/) do |method, source, field, data, identifier|
+  if method == "change"
+    FileUtils.cp(fixture_file(source, ".xml.new"), fixture_file(source))
+  elsif method == "revert"
+    FileUtils.cp(fixture_file(source, ".xml.orig"), fixture_file(source))
   end
 end
 
 And (/^I reload the "(.*?)" source data into Ichabod$/) do |source|
-    Ichabod::DataLoader.new(@file, source).load
+  Ichabod::DataLoader.new(fixture_file(source), source).load
 end
