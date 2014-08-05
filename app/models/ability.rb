3959dcef7ca040a17ccae71762ba689e475bca94
B Alter <barnaby.alter@gmail.com>
2014-08-05 16:57:08 -0400

    Added missing helper file and had to rewrite the ability file to match new concept on permission

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,18 +3,6 @@ class Ability
 
   # Define any customized permissions here.
   def custom_permissions
-    # Limits deleting objects to a the admin user
-    #
-    if current_user.admin?
-      can [:destroy], ActiveFedora::Base
-    end
 
-    #aliasing create/read/update/delete actions 
-    alias_action :create, :read, :update, :destroy, :to => :crud
-    # Limits creating new objects to a specific group
-     if user_groups.include? 'gis_cataloger'
-       can [:crud], Nyucore, type: "Geospatial Data" if user_groups.include? 'gis_cataloger'
-         
-     end
   end
 end
--- spec/support/test_user_helper.rb
+++ spec/support/test_user_helper.rb
@@ -1,13 +1,5 @@
-
 module SpecTestHelper
-
   def create_or_return_test_admin
     @user ||= create(:admin)
   end
-  
-end
-
-
-RSpec.configure do |config|
-  config.include SpecTestHelper, :type => :controller
 end
