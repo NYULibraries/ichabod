c250155d354abf12dbee3eac25bd3cd4f929f49c
B Alter <barnaby.alter@gmail.com>
2014-08-05 17:02:41 -0400

    Had to change some steps to keep cucumber working

--- features/step_definitions/record_authorization_steps.rb
+++ features/step_definitions/record_authorization_steps.rb
@@ -1,29 +1,15 @@
 Then(/^the record should have link "(.*?)"$/) do  |link_text|
   expect(page.all(:xpath, "//a[text()='#{link_text}']").count).to be > 0
- end
+end
 
 Given(/^I view record with id "(.*?)"$/) do |id|
   visit "#{nyucores_path}/#{id}"
 end
 
 Then(/^the record should not have link "(.*?)"$/) do |link_text|
- expect(page.all(:xpath, "//a[text()='#{link_text}']").count).to be 0
+  expect(page.all(:xpath, "//a[text()='#{link_text}']").count).to be 0
 end
 
 Given(/^I am not logged in$/) do
-  ENV['PDS_HANDLE'] = nil
-end
-
-Around('@loggedin_GIS') do |scenario, block|
-  VCR.use_cassette('logged in GIS Cataloger', record: :none) do
-    block.call
-  end
-  ENV['PDS_HANDLE'] = nil
-end
-
-Around('@loggedin_FDA') do |scenario, block|
-  VCR.use_cassette('logged in FDA Cataloger', record: :none) do
-    block.call
-  end
   ENV['PDS_HANDLE'] = nil
 end
