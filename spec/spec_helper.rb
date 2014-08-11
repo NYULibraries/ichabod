11f3b0665358f9476147fcca5545a4860ef36d69
Scot Dalton <scotdalton@gmail.com>
2014-08-11 15:44:41 -0400

    Add the ResourceSet helpers to the spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,6 +39,9 @@ RSpec.configure do |config|
 
   config.include SpecTestHelper, :type => :controller
 
+  # Include ResourceSet macros
+  config.include ResourceSetMacros
+
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
