fd633da85feecda251b58e5c9bc0b609ba3f722c
Scot Dalton <scotdalton@gmail.com>
2014-08-11 14:21:48 -0400

    Add support classes for mocking ResourceSets

---
+++ spec/support/resource_set_mocks.rb
@@ -0,0 +1,14 @@
+module ResourceSetMocks
+  class MockSourceReader < Ichabod::ResourceSet::SourceReader
+    def read
+      5.times.map do |n|
+        FactoryGirl.create :resource, identifier: "#{resource_set.prefix}-n"
+      end
+    end
+  end
+
+  class MockResourceSet < Ichabod::ResourceSet::Base
+    prefix = 'mock'
+    source_reader_class = MockSourceReader
+  end
+end
