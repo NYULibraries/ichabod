b4342c21959c8ffe086b261c1f3cf4a9ab7be5fa
Scot Dalton <scotdalton@gmail.com>
2014-08-12 21:13:55 -0400

    Add a name attribute to ManagementGroups

--- spec/ichabod/resource_set/management_group_spec.rb
+++ spec/ichabod/resource_set/management_group_spec.rb
@@ -2,12 +2,17 @@ require 'spec_helper'
 module Ichabod
   module ResourceSet
     describe ManagementGroup do
+      let(:name) { :gis_cataloger }
       let(:owner) { build(:user) }
       let(:members) { [build(:user), build(:user)] }
       subject(:management_group) do
-        ResourceSet::ManagementGroup.new(owner, *members)
+        ResourceSet::ManagementGroup.new(name, owner, *members)
       end
       it { should be_a ResourceSet::ManagementGroup }
+      describe '#name' do
+        subject { management_group.name }
+        it { should eq name }
+      end
       describe '#owner' do
         subject { management_group.owner }
         it { should eq owner }
