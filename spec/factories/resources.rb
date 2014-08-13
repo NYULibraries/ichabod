44df48f81b5e521de4a3f343d53ca44bf69da409
Scot Dalton <scotdalton@gmail.com>
2014-08-13 15:31:34 -0400

    Resource factory should initialize with Arrays and populate prefix

--- spec/factories/resources.rb
+++ spec/factories/resources.rb
@@ -1,7 +1,8 @@
 FactoryGirl.define do
   factory :resource, class: Ichabod::ResourceSet::Resource do
     skip_create
-    sequence :identifier do |n| "prefix:id-#{n}" end
-    sequence :title do |n| "Title #{n}" end
+    prefix 'prefix'
+    sequence :identifier do |n| ["identifier-#{n}"] end
+    sequence :title do |n| ["Title #{n}"] end
   end
 end
