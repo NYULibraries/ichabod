f1d6c6acc4a48874d863cb4e7a467ecfdfcfd96a
Scot Dalton <scotdalton@gmail.com>
2014-08-14 08:49:35 -0400

    Update the "change data" feature to use the DataLoader with ResourceSets

--- features/support/helpers/data_loader.rb
+++ features/support/helpers/data_loader.rb
@@ -1,6 +1,22 @@
 module IchabodFeatures
   module DataLoader
 
+    def name_for_source(source)
+      source.downcase.gsub(' ', '_').classify
+    end
+
+    def class_for_source(source)
+      name_for_source(source).constantize
+    end
+
+    def instance_for_source(source)
+      class_for_source(source).new
+    end
+
+    def prefix_for_source(source)
+      instance_for_source(source).prefix
+    end
+
     def fixture_file(source, ext = ".xml")
       "#{fixture_dir}/#{source}#{ext}"
     end
@@ -8,6 +24,5 @@ module IchabodFeatures
     def fixture_dir
       Rails.root.join("spec","fixtures")
     end
-
   end
 end
