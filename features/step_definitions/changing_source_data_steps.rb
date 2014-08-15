e67cecd46df9728da7f0840022532e818a298b89
Scot Dalton <scotdalton@gmail.com>
2014-08-15 11:35:07 -0400

    Initialize the DataLoader correctly in the change data source step definitions

--- features/step_definitions/changing_source_data_steps.rb
+++ features/step_definitions/changing_source_data_steps.rb
@@ -10,5 +10,5 @@ end
 And(/^I reload the "(.*?)" source data into Ichabod$/) do |source|
   name = name_for_source(source)
   prefix = prefix_for_source(source)
-  Ichabod::DataLoader.new(name, filename: fixture_file("#{prefix}_tmp")).load
+  Ichabod::DataLoader.new(name, fixture_file("#{prefix}_tmp")).load
 end
