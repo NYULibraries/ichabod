371b51eb496b367afe4c61e2db7910da8a196648
Scot Dalton <scotdalton@gmail.com>
2014-08-15 10:53:48 -0400

    Mock the call to the file name in the OaiDcFileReader spec since we don't really care how it's implemented, just that it works

--- spec/ichabod/resource_set/source_readers/oai_dc_file_reader_spec.rb
+++ spec/ichabod/resource_set/source_readers/oai_dc_file_reader_spec.rb
@@ -4,7 +4,10 @@ module Ichabod
     module SourceReaders
       describe OaiDcFileReader do
         let(:oai_dc_filename) { './spec/fixtures/sample_sdr.xml' }
-        let(:resource_set) { mock_resource_set(filename: oai_dc_filename) }
+        let(:resource_set) { mock_resource_set }
+        before do
+          allow(resource_set).to receive(:filename) { oai_dc_filename }
+        end
         subject(:oai_dc_file_reader) { OaiDcFileReader.new(resource_set) }
         it { should be_a OaiDcFileReader }
         it { should be_a ResourceSet::SourceReader }
@@ -13,48 +16,28 @@ module Ichabod
           it { should eq resource_set }
         end
         describe '#read' do
-          let(:resource) { subject.first }
-          subject { oai_dc_file_reader.read }
+          subject(:read) { oai_dc_file_reader.read }
           it { should be_an Array }
           it { should_not be_empty }
-          it 'should contain 1 record' do
-            expect(subject.size).to eq 1
-          end
+          its(:size) { should eq 1 }
           it 'should include only ResourceSet::Resources' do
             subject.each do |resource|
               expect(resource).to be_a ResourceSet::Resource
             end
           end
-          context 'when the file is an SDR file' do
-            it 'should have a resource with a prefix "mock"' do
-              expect(resource.prefix).to include 'mock'
-            end
-            it 'should have a resource with an identifier "DSS.NYCDCP_MONKEY_LION\DSS.Lion_Monkey"' do
-              expect(resource.identifier).to include 'DSS.NYCDCP_MONKEY_LION\DSS.Lion_Monkey'
-            end
-            it 'should have a resource with a title "LION"' do
-              expect(resource.title).to include 'LION'
-            end
-            it 'should have a resource with a publisher "New York City Dept. of City Planning"' do
-              expect(resource.publisher).to include 'New York City Dept. of City Planning'
-            end
-            it 'should have a resource with an available "http://magellan.home.nyu.edu/datasets/zips/NYCDCP_DCPLION_10CAV-Lion_GJK.zip"' do
-              expect(resource.available).to include 'http://magellan.home.nyu.edu/datasets/zips/NYCDCP_DCPLION_10CAV-Lion_GJK.zip'
-            end
-            it 'should have a resource with a type "Geospatial Data"' do
-              expect(resource.type).to include 'Geospatial Data'
-            end
-            it 'should have a resource with a description "LION is a single line representation of New York City streets containing address ranges and other information."' do
-              expect(resource.description).to include 'LION is a single line representation of New York City streets containing address ranges and other information.'
-            end
-            it 'should have a resource with an edition "10C"' do
-              expect(resource.edition).to include '10C'
-            end
-            it 'should have a resource with an series "NYCDCP_DCPLION_10CAV"' do
-              expect(resource.series).to include 'NYCDCP_DCPLION_10CAV'
-            end
-            it 'should have a resource with an version "DSS.NYCDCP_DCPLION_10cav\DSS.Lion_GJK"' do
-              expect(resource.version).to include 'DSS.NYCDCP_DCPLION_10cav\DSS.Lion_GJK'
+          describe 'the first record' do
+            subject { read.first }
+            context 'when the file is an SDR file' do
+              its(:prefix) { should eq resource_set.prefix }
+              its(:identifier) { should include 'DSS.NYCDCP_MONKEY_LION\DSS.Lion_Monkey' }
+              its(:title) { should include 'LION' }
+              its(:publisher) { should include 'New York City Dept. of City Planning' }
+              its(:available) { should include 'http://magellan.home.nyu.edu/datasets/zips/NYCDCP_DCPLION_10CAV-Lion_GJK.zip' }
+              its(:type) { should include 'Geospatial Data' }
+              its(:description) { should include 'LION is a single line representation of New York City streets containing address ranges and other information.' }
+              its(:edition) { should include '10C' }
+              its(:series) { should include 'NYCDCP_DCPLION_10CAV' }
+              its(:version) { should include 'DSS.NYCDCP_DCPLION_10cav\DSS.Lion_GJK' }
             end
           end
         end
