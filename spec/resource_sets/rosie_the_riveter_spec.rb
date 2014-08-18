0e2c1cb0034d0100a0c50e468c8e87f8ba0919d0
Scot Dalton <scotdalton@gmail.com>
2014-08-18 16:41:15 -0400

    Add the RosieTheRiveter ResourceSet for load up Resources from the Rosie the Riveter collection

--- spec/resource_sets/rosie_the_riveter_spec.rb
+++ spec/resource_sets/rosie_the_riveter_spec.rb
@@ -1,14 +1,35 @@
 require 'spec_helper'
 describe RosieTheRiveter do
   let(:prefix) { 'rosie' }
-  let(:url) { 'https://rosie.the.riveter' }
-  subject(:rosie_the_riveter) { RosieTheRiveter.new(url) }
+  let(:endpoint_url) { 'https://rosie.the.riveter' }
+  let(:user) { 'user' }
+  let(:password) { 'password' }
+  let(:collection_code) { 'rosie' }
+  let(:args) { [endpoint_url, user, password].compact }
+  subject(:rosie_the_riveter) { RosieTheRiveter.new(*args) }
   it { should be_a RosieTheRiveter }
   it { should be_a Ichabod::ResourceSet::Base }
-  its(:url) { should eq url }
   its(:prefix) { should eq prefix }
+  its(:endpoint_url) { should eq endpoint_url }
+  its(:user) { should eq user }
+  its(:password) { should eq password }
+  its(:collection_code) { should eq collection_code }
   its(:editors) { should eq ['admin_group'] }
   its(:before_creates) { should eq [:add_edit_groups] }
+  context 'when initialized without a password' do
+    let(:password) { nil }
+    it 'should not raise an error' do
+      expect { subject }.not_to raise_error
+    end
+    its(:password) { should be_nil }
+    context 'and initialized without a user' do
+      let(:user) { nil }
+      it 'should not raise an error' do
+        expect { subject }.not_to raise_error
+      end
+      its(:user) { should be_nil }
+    end
+  end
   describe '.prefix' do
     subject { RosieTheRiveter.prefix }
     it { should eq prefix }
@@ -16,6 +37,7 @@ describe RosieTheRiveter do
   describe '.source_reader' do
     subject { RosieTheRiveter.source_reader }
     it 'should be a DLTS reader'
+    it { should eq Ichabod::ResourceSet::SourceReaders::RosieTheRiveterReader }
   end
   describe '.editors' do
     subject { RosieTheRiveter.editors }
