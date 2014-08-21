6fc92aee0c3ba1d0e756bd14acc4da0e7ef9467c
Corey Harper <corey.harper@gmail.com>
2014-08-21 16:07:50 -0400

    Update views to handle multiple datastreams

--- spec/views/nyucores/edit.html.erb_spec.rb
+++ spec/views/nyucores/edit.html.erb_spec.rb
@@ -1,14 +1,16 @@
 require 'spec_helper'
 
 describe "nyucores/edit", vcr: { cassette_name: "nyucore show edit form" } do
-  let(:item) { create(:nyucore) }
+  let(:user) { create(:user) }
+  let(:item) { create(:valid_nyucore) }
   before(:each) { @item = item }
+  before(:each) { controller.stub(:current_user).and_return(user) }
 
   it "should render the edit nyucore form" do
     render
 
     assert_select "form[action=?][method=?]", nyucore_path(item), "post" do
-      assert_select "input#nyucore_title[name=?]", "nyucore[title]"
+      assert_select "input#nyucore_title_native_metadata[name=?]", "nyucore[title][]"
     end
   end
 end
