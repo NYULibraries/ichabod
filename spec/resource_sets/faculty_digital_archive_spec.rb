500b32f077c3547d8921e52945264191629dc224
Scot Dalton <scotdalton@gmail.com>
2014-08-26 15:56:26 -0400

    In the FacultyDigitalArchive ResourceSet, set the "http" identifier before we create the Nyucore model

--- spec/resource_sets/faculty_digital_archive_spec.rb
+++ spec/resource_sets/faculty_digital_archive_spec.rb
@@ -7,7 +7,7 @@ describe FacultyDigitalArchive do
   it { should be_a Ichabod::ResourceSet::Base }
   its(:filename) { should eq filename }
   its(:editors) { should eq ['admin_group', 'fda_cataloger'] }
-  its(:before_creates) { should eq [:add_edit_groups, :add_identifier_as_available_or_citation] }
+  its(:before_creates) { should eq [:add_edit_groups, :add_identifier_as_available_or_citation, :set_http_identifier] }
   describe '.prefix' do
     subject { FacultyDigitalArchive.prefix }
     it { should eq prefix }
@@ -22,7 +22,7 @@ describe FacultyDigitalArchive do
   end
   describe '.before_creates' do
     subject { FacultyDigitalArchive.before_creates }
-    it { should eq [:add_edit_groups, :add_http_identifier_as_available_and_citation] }
+    it { should eq [:add_edit_groups, :add_identifier_as_available_or_citation, :set_http_identifier] }
   end
   describe '#create', vcr: {cassette_name: 'resource sets/fda create'} do
     subject(:create) { faculty_digital_archive.create }
