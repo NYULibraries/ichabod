d9f948ae63e1b98a4b20ea581f949b0f46724852
Scot Dalton <scotdalton@gmail.com>
2014-08-26 15:56:26 -0400

    In the FacultyDigitalArchive ResourceSet, set the "http" identifier before we create the Nyucore model

--- app/resource_sets/faculty_digital_archive.rb
+++ app/resource_sets/faculty_digital_archive.rb
@@ -2,7 +2,7 @@ class FacultyDigitalArchive < Ichabod::ResourceSet::Base
   self.prefix = 'fda'
   self.source_reader = :oai_dc_file_reader
   editor :fda_cataloger
-  before_create :add_identifier_as_available_or_citation
+  before_create :add_identifier_as_available_or_citation, :set_http_identifier
 
   attr_reader :filename
 
@@ -20,6 +20,14 @@ class FacultyDigitalArchive < Ichabod::ResourceSet::Base
       else
         nyucore.citation = id
       end
+    end
+  end
+
+  def set_http_identifier(*args)
+    resource, nyucore = *args
+    identifiers = resource.identifier
+    nyucore.identifier = identifiers.find do |identifier|
+      identifier.starts_with? 'http://'
     end
   end
 end
