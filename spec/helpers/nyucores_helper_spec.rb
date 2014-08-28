64256f0213736fb170b384d312d90d588ccd7055
Scot Dalton <scotdalton@gmail.com>
2014-08-28 17:57:07 -0400

    Update the Nyucore helper to help with the form

--- spec/helpers/nyucores_helper_spec.rb
+++ spec/helpers/nyucores_helper_spec.rb
@@ -1,42 +1,60 @@
 require "spec_helper"
-
 describe NyucoresHelper do
+  let(:item) { build(:nyucore) }
+  before { assign(:item, item) }
 
-  include NyucoresHelper
-
-  describe ".multivalue_fields" do
-    subject { multivalue_fields }
-    it { should be_instance_of Array }
+  describe '#fields' do
+    subject { helper.fields }
+    it { should be_an Array }
   end
 
-  describe ".format_field_index" do
+  describe '#values' do
+    subject { helper.values(:title) }
+    it { should be_an Array }
+  end
+
+  describe '#multiple?' do
+    subject { helper.multiple?(:title) }
+    it { should be true }
+  end
+
+  describe '#source?' do
+    subject { helper.source?(:title, item.title.first) }
+    it { should be false }
+  end
+
+  describe '#editable?' do
+    subject { helper.editable?(:title, item.title.first) }
+    it { should be true }
+  end
+
+  describe '#nyucore_presenter' do
+    subject { helper.nyucore_presenter }
+    it { should be_an NyucorePresenter }
+  end
+
+  describe '#format_field_index' do
     let(:field) { [] }
     let(:index) { nil }
-
-    subject { format_field_index(field, index) }
-
+    subject { helper.format_field_index(field, index) }
     context "when index is nil and hence called by the last element" do
       context "when field array is blank" do
         it { should be_nil }
       end
-
       context "when field array contains a single value" do
         let(:field) { ["Value1"] }
         it { should be 1 }
       end
-
       context "when field array contains multiple values" do
         let(:field) { ["Value1","Value2"] }
         it { should be 2 }
       end
     end
-
     context "when index is not nil and hence not the last element" do
       let(:index) { 2 }
       context "when field array is blank" do
         it { should be_nil }
       end
-
       context "when field array contains values" do
         let(:field) { ["Value1","Value2","Value3"] }
         it { should be 2 }
@@ -44,4 +62,8 @@ describe NyucoresHelper do
     end
   end
 
+  describe '#field_id' do
+    subject { helper.field_id(:title, 0) }
+    it { should eq 'nyucore_title' }
+  end
 end
