9fd863fe652b26e472f66c76e497cb2938f098bf
Scot Dalton <scotdalton@gmail.com>
2014-08-28 17:57:07 -0400

    Update the Nyucore helper to help with the form

--- app/helpers/nyucores_helper.rb
+++ app/helpers/nyucores_helper.rb
@@ -1,9 +1,8 @@
 module NyucoresHelper
 
-  # List of fields that have multiple values, so we can just loop in the view
-  # TODO: Encapsulate this somewhere else
-  def multivalue_fields
-    @multivalue_fields ||= [:available, :description, :edition, :series, :version, :date, :format, :language, :relation, :rights, :subject, :citation]
+  # List of Nyucore fields
+  def fields
+    @fields ||= Nyucore::FIELDS[:single] + Nyucore::FIELDS[:multiple] + Nyucore::EXTRAS
   end
 
   # Retrieve the index of the current field so we can append that to the element's ID
@@ -31,25 +30,15 @@ module NyucoresHelper
       return index if index.to_i > 0
     end
   end
-  def item_fields_for_datastream(ds, field)
-    item_fields = []
-#binding.pry
-    if item_has_field?(ds, field)
-      item_fields |= (item_field_is_array?(ds, field)) ? @item.send(ds.first).send(field) : [@item.send(ds.first).send(field)]
-    end
-    item_fields
+
+  extend Forwardable
+  def_delegators :nyucore_presenter, :values, :source?, :editable?, :multiple?
+
+  def nyucore_presenter
+    @nyucore_presenter ||= NyucorePresenter.new(@item)
   end
 
-  def item_has_field?(ds, field)
-    defined? @item.send(ds.first).send(field).present?
-  end
-
-  def item_field_is_array?(ds, field)
-    @item.send(ds.first).send(field).is_a? Array
-  end
-
-  def nyucore_form_id(field, ds, index=nil)
-#binding.pry
-    "nyucore_#{field.to_s}_#{ds.to_s}#{format_field_index(@item.send(ds).send(field), index)}"
+  def field_id(field, index = nil)
+    "nyucore_#{field}#{format_field_index(values(field), index)}"
   end
 end
