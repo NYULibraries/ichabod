45d293975b1faf0c27d12e2a004dbcbd8c279b07
Scot Dalton <scotdalton@gmail.com>
2014-09-08 21:00:21 -0400

    Update the Nyucore field constants
    The updated constants are aggregated in the FIELDS constant and include the :resource_set attribute. The constants that make up FIELDS are NYUCORE_FIELDS, EXTRA_SINGLE and EXTRA_MULTIPLES. SINGLE_FIELDS and MULTIPLE_FIELDS are also defined for convenience.

--- app/models/nyucore.rb
+++ app/models/nyucore.rb
@@ -1,13 +1,17 @@
 class Nyucore < ActiveFedora::Base
   include Hydra::AccessControls::Permissions
 
-  FIELDS = {
+  NYUCORE_FIELDS = {
     :multiple => [:available, :citation, :title, :creator, :type, :publisher,
                   :description, :edition, :date, :format, :language, :relation,
                   :rights, :subject, :series, :version],
     :single => [:identifier]
   }
-  EXTRAS = [:addinfolink, :addinfotext]
+  EXTRA_SINGLES = [:resource_set]
+  EXTRA_MULTIPLES = [:addinfolink, :addinfotext]
+  SINGLE_FIELDS = NYUCORE_FIELDS[:single] + EXTRA_SINGLES
+  MULTIPLE_FIELDS = NYUCORE_FIELDS[:multiple] + EXTRA_MULTIPLES
+  FIELDS = SINGLE_FIELDS + MULTIPLE_FIELDS
   METADATA_STREAMS = ['source_metadata', 'native_metadata']
 
   # Add multiple metadata streams to the model and include the attributes we
@@ -16,9 +20,8 @@ class Nyucore < ActiveFedora::Base
   # our base.
   METADATA_STREAMS.each do |metadata_stream|
     has_metadata metadata_stream, type: Ichabod::NyucoreDatastream
-    has_attributes *FIELDS[:single], datastream: metadata_stream, multiple: false
-    has_attributes *FIELDS[:multiple], datastream: metadata_stream, multiple: true
-    has_attributes *EXTRAS, datastream: metadata_stream, multiple: true
+    has_attributes(*SINGLE_FIELDS, datastream: metadata_stream, multiple: false)
+    has_attributes(*MULTIPLE_FIELDS, datastream: metadata_stream, multiple: true)
   end
 
   # Override the attribute writers by delegating to the native_metadata
@@ -40,7 +43,7 @@ class Nyucore < ActiveFedora::Base
   #   nyucore.title
   #   # => ['Source Title', 'Native Title']
   extend Forwardable
-  (FIELDS[:single] + FIELDS[:multiple] + EXTRAS).each do |field|
+  FIELDS.each do |field|
     def_delegators :native_metadata, "#{field}=".to_sym
   end
 
@@ -59,7 +62,7 @@ class Nyucore < ActiveFedora::Base
   #   # => nil
   #   nyucore.title
   #   # => ['Source Title', 'Native Title']
-  (FIELDS[:multiple] + EXTRAS).each do |field|
+  MULTIPLE_FIELDS.each do |field|
     define_method(field) do
       source_metadata.send(field).each_with_index.collect do |value, index|
         Metadatum.new(value, source_metadata, index)
@@ -83,7 +86,7 @@ class Nyucore < ActiveFedora::Base
   #   # => nil
   #   nyucore.identifier
   #   # => 'source_identifier'
-  FIELDS[:single].each do |field|
+  SINGLE_FIELDS.each do |field|
     define_method(field) do
       # ActiveFedora forces the first value and so do we.
       # https://github.com/projecthydra/active_fedora/blob/v6.7.6/lib/active_fedora/attributes.rb#L153
