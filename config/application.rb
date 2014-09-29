0ab113038f1ed0a35a262417e1bb8650a0600c86
B Alter <barnaby.alter@gmail.com>
2014-09-29 14:58:49 -0400

    Make sure we precompile the correct font files for BS3 glyphicons

--- config/application.rb
+++ config/application.rb
@@ -31,8 +31,8 @@ module Ichabod
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
 
-    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    # config.i18n.default_locale = :de
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif *.woff *.ttf *.svg *.eot)
   end
 end
