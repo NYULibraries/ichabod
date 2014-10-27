b5d999166d7ba5f630c7a70248893956387bb682
jgpawletko <jgpawletko@gmail.com>
2014-10-27 16:46:21 -0400

    add example for Archive of Contemporary Composers' Websites
    Add example to test for presence in Collection map.
    Update collection title (add apostrophe after "Composers").

--- spec/models/nyucore/collections_spec.rb
+++ spec/models/nyucore/collections_spec.rb
@@ -70,5 +70,11 @@ describe Nyucore::Collections do
       let(:resource_set) { ['voice'] }
       it { should eql ['Voices of the Food Revolution'] }
     end
+    context "when object is from the Archive of Contemporary Composers' Websites resource set" do
+      let(:type) { nil }
+      let(:publisher) { nil }
+      let(:resource_set) { ['archive_it_accw'] }
+      it { should eql ["Archive of Contemporary Composers' Websites"] }
+    end
   end
 end
