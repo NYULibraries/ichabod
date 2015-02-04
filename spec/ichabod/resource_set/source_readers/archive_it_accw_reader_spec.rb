f6bbb638717a066324aa72356f91bb53753ffff1
jgpawletko <jgpawletko@gmail.com>
2015-02-03 23:21:14 -0500

    update ArchiveItAccw source reader
    Update ArchiveItAccw source reader to get path from resource set.
    Previously, the path was hardcoded.

--- spec/ichabod/resource_set/source_readers/archive_it_accw_reader_spec.rb
+++ spec/ichabod/resource_set/source_readers/archive_it_accw_reader_spec.rb
@@ -4,10 +4,12 @@ module Ichabod
     module SourceReaders
       describe ArchiveItAccwReader do
         let(:endpoint_url) { 'https://archive-it.org' }
+        let(:path) { '/collections/4049.json' }
         let(:collection_code) { 'archive_it_accw' }
         let(:resource_set) { mock_resource_set }
         before do
           allow(resource_set).to receive(:endpoint_url) { endpoint_url }
+          allow(resource_set).to receive(:path) { path }
           allow(resource_set).to receive(:collection_code) { collection_code }
         end
         subject(:reader) { ArchiveItAccwReader.new(resource_set) }
