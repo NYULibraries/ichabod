dbc3d94c437f70ad3b9d64e5e5b0de2698e302ca
ekate <ekate@nyu.edu>
2015-05-05 20:05:22 -0400

    Added collection model

---
+++ app/models/collection.rb
@@ -0,0 +1,21 @@
+class Collection < ActiveFedora::NtriplesRDFDatastream
+  include Hydra::AccessControls::Permissions
+
+    TERMS = {
+      RDF::DC => [:identifier, :title, :creator, :publisher,
+         :description, :rights],
+
+      NyucoreMetadata::Vocabulary => [:available]
+
+    }
+
+  TERMS.each_pair do |vocabulary, terms|
+      terms.each do |term|
+        property term, predicate: vocabulary.send(term) do |index|
+          index.as(*index_args)
+        end
+      end
+    end
+
+  
+end
