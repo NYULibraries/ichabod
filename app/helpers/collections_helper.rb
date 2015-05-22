ccc697b08a065e2f2d1bef78975e144f810f41e8
ekate <ekate@nyu.edu>
2015-05-21 22:43:51 -0400

    Modified collection model

--- app/helpers/collections_helper.rb
+++ app/helpers/collections_helper.rb
@@ -1,2 +1,19 @@
 module CollectionsHelper
+  def single_fields
+     @single_fields ||= Collection::SINGLE_FIELDS
+  end
+
+
+  def multiple_fields
+     @multiple_fields ||= Collection::MULTIPLE_FIELDS
+  end
+
+  def get_boolean(value)
+     @checked = value.blank? ? true:false
+  end
+
+  def get_required(field)
+     @required= field.to_s=="title" ? true:false
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,10 @@ Ichabod::Application.routes.draw do
   get 'about', :to => "catalog#index"
   Blacklight.add_routes(self)
 
+  get 'collections', :to => "collections#index"
   get 'login', :to => 'user_sessions#new', :as => :login
   get 'logout', :to => 'user_sessions#destroy', :as => :logout
   get 'validate', :to => 'user_sessions#validate', :as => :validate
   resources :user_sessions
-
+ 
 end
