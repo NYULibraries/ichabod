a7de6f0af716874742b423cb7878583775d4ff95
ekate <ekate@nyu.edu>
2015-05-22 13:55:05 -0400

    Modified specs

--- spec/views/collections/new.html.erb_spec.rb
+++ spec/views/collections/new.html.erb_spec.rb
@@ -1,30 +1,13 @@
 require 'spec_helper'
-
-describe "collections/new" do
-  before(:each) do
-    assign(:collection, stub_model(Collection,
-      :identifier => "MyString",
-      :title => "MyString",
-      :creator => "MyString",
-      :publisher => "MyString",
-      :description => "MyString",
-      :available => "MyString",
-      :rights => "MyString"
-    ).as_new_record)
+describe 'collections/new' do
+  let(:user) { build(:user) }
+  let(:collection) { build(:collection) }
+  before do
+    allow(controller).to receive(:current_user).and_return(user)
+    assign(:collection, collection)
   end
-
-  it "renders new collection form" do
+  it 'should render the new collection form' do
     render
-
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "form[action=?][method=?]", collections_path, "post" do
-      assert_select "input#collection_identifier[name=?]", "collection[identifier]"
-      assert_select "input#collection_title[name=?]", "collection[title]"
-      assert_select "input#collection_creator[name=?]", "collection[creator]"
-      assert_select "input#collection_publisher[name=?]", "collection[publisher]"
-      assert_select "input#collection_description[name=?]", "collection[description]"
-      assert_select "input#collection_available[name=?]", "collection[available]"
-      assert_select "input#collection_rights[name=?]", "collection[rights]"
-    end
+    expect(rendered).to match /New collection/
   end
 end
