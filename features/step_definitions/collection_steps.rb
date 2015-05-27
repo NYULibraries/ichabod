1d4565f4b519c1bcea84b00dcdf3a814d99b906b
ekate <ekate@nyu.edu>
2015-05-26 20:50:08 -0400

    Added cucs

---
+++ features/step_definitions/collection_steps.rb
@@ -0,0 +1,27 @@
+Given(/^I am on the "New Collection" form$/) do
+   visit new_collection_path
+end
+
+Given(/^the collection "(.*?)" exists$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I am on the "(.*?)" form for "(.*?)"$/) do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I am on the collections list$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I click on the "(.*?)" link for "(.*?)"$/) do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the collection should be deleted$/) do
+  pending # express the regexp above with the code you wish you had
+end
