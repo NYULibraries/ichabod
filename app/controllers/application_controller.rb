88e2f1e598d0e0c56d7c2ff1ee42637cefa25cc2
hab278 <hab278@nyu.edu>
2015-07-08 17:38:06 -0400

    Use the new NYULibraries-assets's InstitutionHelper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
 
+  include Nyulibraries::Assets::InstitutionsHelper
   # Adds a few additional behaviors into the application controller
    include Blacklight::Controller
   # Please be sure to impelement current_user and user_session. Blacklight depends on
