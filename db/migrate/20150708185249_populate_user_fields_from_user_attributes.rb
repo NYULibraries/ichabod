9c109eadc64270898668996f314104e4a165b0a5
hab278 <hab278@nyu.edu>
2015-07-09 14:33:27 -0400

    Accidently called a field by the wrong name

--- db/migrate/20150708185249_populate_user_fields_from_user_attributes.rb
+++ db/migrate/20150708185249_populate_user_fields_from_user_attributes.rb
@@ -4,7 +4,7 @@ class PopulateUserFieldsFromUserAttributes < ActiveRecord::Migration
       User.class_eval { serialize :user_attributes }
       User.all.each do |user|
         user.update_attribute :aleph_id, user.user_attributes[:nyuidn]
-        user.update_attribute :status, user.user_attributes[:bor_status]
+        user.update_attribute :patron_status, user.user_attributes[:bor_status]
         user.update_attribute :institution_code, user.user_attributes[:primary_institution]
       end
     end
