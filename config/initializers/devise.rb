f9c7e0eb040e4fe6185efc3647ca9eb158d4acc9
hab278 <hab278@nyu.edu>
2015-07-09 16:10:49 -0400

    In ichabod, we call it SECRET_KEY_BASE, not SECRET_TOKEN

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,7 +1,7 @@
 Devise.setup do |config|
   config.mailer_sender = 'no-reply@library.nyu.edu'
   require 'devise/orm/active_record'
-  config.secret_key = ENV['SECRET_TOKEN']
+  config.secret_key = ENV['SECRET_KEY_BASE']
   config.strip_whitespace_keys = [ :email ]
   config.skip_session_storage = [:http_auth]
   config.stretches = Rails.env.test? ? 1 : 10
