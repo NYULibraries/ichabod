4d4acbe57ba98594f7458642f58c975055b443c2
Esha Datta <eshadatta16@gmail.com>
2015-08-20 16:52:41 -0400

    got most of the mapping

--- lib/ichabod/resource_set/source_readers/json_loader.rb
+++ lib/ichabod/resource_set/source_readers/json_loader.rb
@@ -6,14 +6,28 @@ module Ichabod
       require 'multi_json'    
      
         def read
-          entities
-          binding.pry
+          entities.collect do |entity|
+            ResourceSet::Resource.new(resource_attributes_from_entities(entity))
+          end
         end
 
         private
         extend Forwardable
-        def_delegators :resource_set, :endpoint_url, :rsp_field, :each_rec_field, :datasource_params, :authentication
+        def_delegators :resource_set, :endpoint_url, :rsp_field, :each_rec_field, :datasource_params, :authentication, :set_data_map
 
+        def resource_attributes_from_entities(entity)
+          hsh = {}
+          hsh[:prefix] = resource_set.prefix
+          set_data_map.each_pair { |k,v|
+            hsh[k] = entity[parse_data_map(v)] 
+                binding.pry if k =~ /type/
+          }
+          hsh
+           binding.pry
+        end
+        def parse_data_map(map)
+          map.is_a?(Array) ? map[0] : map 
+        end
         def entities
           @entities ||= datasource_response[each_rec_field]
         end  
