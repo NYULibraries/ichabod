6711ca1349b6ed12849d4bf9dab9c34ba6999476
Esha Datta <eshadatta16@gmail.com>
2015-09-09 14:48:17 -0400

    using generic json loader for nyu press and masses

--- app/resource_sets/nyu_press_open_access_book.rb
+++ app/resource_sets/nyu_press_open_access_book.rb
@@ -1,14 +1,36 @@
 class NyuPressOpenAccessBook < Ichabod::ResourceSet::Base
   self.prefix = 'nyupress'
-  self.source_reader = :nyu_press_open_access_book_reader
+  self.source_reader = :json_loader
+  FORMAT = "Book"
 
-  attr_reader :datasource_params, :authentication, :rsp_field, :each_rec_field, :set_data_map
+  attr_reader :endpoint_url, :datasource_params, :rsp_field, :each_rec_field, :authentication, :set_data_map
   alias_method :collection_code, :prefix
 
   def initialize(*args)
     @endpoint_url = args.shift
-    @start = args.shift || '0'
-    @rows = args.shift || '65'
+    start = args.shift || '0'
+    rows = args.shift || '65'
+    @datasource_params = {start: start, rows: rows, wt: 'json'}
+    @rsp_field = 'response'
+    @each_rec_field = 'docs'
+    @set_data_map = set_map_nyu_press
     super
   end
+
+  private
+
+  def set_map_nyu_press
+   {
+      identifier: ['id'],
+      isbn: ['id'],
+      title: ['title'],
+      available: ['handle'],
+      citation: ['handle'],
+      date: ['date'],
+      description: ['description'],
+      type: FORMAT,
+      language: ['language'],
+      format: FORMAT
+    }
+ end
 end
