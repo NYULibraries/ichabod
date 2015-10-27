2f1d3f893f490b023fc175109f84b0460b57101a
Esha Datta <eshadatta16@gmail.com>
2015-10-27 14:57:43 -0400

    checking in collection change

--- app/models/nyucore/collections.rb
+++ app/models/nyucore/collections.rb
@@ -6,6 +6,7 @@ class Nyucore < ActiveFedora::Base
       'The Real Rosie the Riveter' => ->(nyucore){ nyucore.resource_set == 'rosie_the_riveter' },
       'Voices of the Food Revolution' => ->(nyucore){ nyucore.resource_set == 'voice' },
       'Research Guides' => ->(nyucore){ nyucore.resource_set == 'lib_guides' },
+      'Masses' => ->(nyucore){ nyucore.resource_set == 'masses' },
       "Spatial Data Repository" => ->(nyucore){ nyucore.type.include? "Geospatial Data" },
       "Archive of Contemporary Composers' Websites" => ->(nyucore){ nyucore.resource_set == 'archive_it_accw' },
       'Asian NGOs Reports' => ->(nyucore){ nyucore.resource_set == 'faculty_digital_archive_ngo'},
