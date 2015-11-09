062593a877617eee3ea10039ea49ca64fe92b835
Esha Datta <eshadatta16@gmail.com>
2015-11-09 14:34:02 -0500

    went through the files with coveralls help. Got the rest of the unused steps

--- features/step_definitions/display_steps.rb
+++ features/step_definitions/display_steps.rb
@@ -3,11 +3,6 @@ Then(/^I should get "(.*?)" field in all results$/) do |field|
   expect(documents_list_container.all('dt', :text => field)).to have_exactly(10).items
 end
 
-Then(/^I should get "(.*?)" field and its corresponding value "(.*?)" in all results$/) do |field,value|
-  expect(documents_list_container.all('dt', :text => field)).to have_exactly(10).items
-  expect(documents_list_container.all('dd', :text => value)).to have_exactly(10).items
- 
-end
 
 And(/^I navigate to details display of the first result$/) do
   within(documents_list_container) do
@@ -19,9 +14,6 @@ Then(/^I should get "(.*?)" field in the details display$/) do |field|
   expect(document_container.all('dt', :text => field)).to have_exactly(1).item
 end
 
-Then(/^I should see the link "(.*?)" in the "(.*?)" field$/) do |link, field|
-  expect(document_field_value(field)).to have_link(link)
-end
 
 Then(/^I should see the value "(.*?)" in the "(.*?)" field$/) do |value, field|
     expect(document_field_value(field)).to have_content(value)
