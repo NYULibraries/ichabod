1f5e42892fdb951bc1c6cb759cd7c919451580be
Esha Datta <eshadatta16@gmail.com>
2015-12-07 15:50:04 -0500

    added more metadata fields and a yml file mapping archives to the more user friendly string value

--- app/resource_sets/woj.rb
+++ app/resource_sets/woj.rb
@@ -2,10 +2,11 @@ class Woj < Ichabod::ResourceSet::Base
   self.prefix = 'woj'
   self.source_reader = :fab_reader
 
-  attr_reader :endpoint_url, :page
+  attr_reader :endpoint_url, :page, :query
   alias_method :collection_code, :prefix
 
   def initialize(url, *args)
+    @query = "?f%5Bcollection_sim%5D%5B%5D=David+Wojnarowicz+Papers&f%5Bdao_sim%5D%5B%5D=Online+Access&f%5Bformat_sim%5D%5B%5D=Archival+Object"
     @endpoint_url = url
     @page = args.shift
     super
