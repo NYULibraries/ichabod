cd7b5ee3bd0edb854edeffef82f1d457e90bfea2
Esha Datta <eshadatta16@gmail.com>
2015-12-08 12:30:32 -0500

    adding back the masses collection. I had rebased and for some reason it disappeared. It's back

--- app/models/nyucore/collections.rb
+++ app/models/nyucore/collections.rb
@@ -11,7 +11,8 @@ class Nyucore < ActiveFedora::Base
       'South Asian NGO and other reports' => ->(nyucore){ nyucore.resource_set == 'faculty_digital_archive_ngo'},
       'NYU Press Open Access Books' => ->(nyucore){ nyucore.resource_set == 'nyu_press_open_access_book' },
       'Data Services' => ->(nyucore){ nyucore.resource_set == 'faculty_digital_archive_service_data' },
-      'David Wojnarowicz Papers' => ->(nyucore){ nyucore.resource_set == 'woj' }
+      'David Wojnarowicz Papers' => ->(nyucore){ nyucore.resource_set == 'woj' },
+      'The Masses' => ->(nyucore){ nyucore.resource_set == 'masses' }
     }
 
     attr_reader :nyucore
