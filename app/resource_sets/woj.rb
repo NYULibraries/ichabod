3890bbdc46c9c2f1fc799f295c5068df989a8ab0
Esha Datta <eshadatta16@gmail.com>
2015-12-08 11:45:46 -0500

    changed where I put the endpoint urls because it's going to be finding aids bridge centric and not finding aids centric, updated tests

--- app/resource_sets/woj.rb
+++ app/resource_sets/woj.rb
@@ -2,12 +2,11 @@ class Woj < Ichabod::ResourceSet::Base
   self.prefix = 'woj'
   self.source_reader = :fab_reader
 
-  attr_reader :endpoint_url, :page, :query
+  attr_reader :page, :query
   alias_method :collection_code, :prefix
 
-  def initialize(url, *args)
+  def initialize(*args)
     @query = "?f%5Bcollection_sim%5D%5B%5D=David+Wojnarowicz+Papers&f%5Bdao_sim%5D%5B%5D=Online+Access&f%5Bformat_sim%5D%5B%5D=Archival+Object"
-    @endpoint_url = url
     @page = args.shift
     super
   end
