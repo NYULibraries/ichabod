ed0ac2014df4efc1ce4d6f57db37a600128a3da0
Esha Datta <eshadatta16@gmail.com>
2015-12-22 13:24:32 -0500

    updated woj rspec to reflect updated code in woj model

--- spec/resource_sets/woj_spec.rb
+++ spec/resource_sets/woj_spec.rb
@@ -3,13 +3,14 @@ describe Woj do
   let(:prefix) { 'woj' }
   let(:page) { nil }
   let(:collection_code) { 'woj' }
+  let(:d_params) { {"f[collection_sim][]" => 'David Wojnarowicz Papers', "f[dao_sim][]" => 'Online Access',"f[format_sim][]"     => 'Archival Object'} }
   let(:args) { nil }
   subject { Woj.new(*args) }
   it { should be_a Woj }
   it { should be_a Ichabod::ResourceSet::Base }
   its(:prefix) { should eq prefix }
   its(:page) { should eq nil }
-  its(:query) { should eq '?f%5Bcollection_sim%5D%5B%5D=David+Wojnarowicz+Papers&f%5Bdao_sim%5D%5B%5D=Online+Access&f%5Bformat_sim%5D%5B%5D=Archival+Object' }
+  its(:data_params) { should eq d_params }
   its(:editors) { should eq ['admin_group'] }
   its(:before_loads) { should eq [:add_edit_groups, :add_resource_set] }
   describe '.prefix' do
