67ae65483d9a8c8ad6613154a7b596fda7c2449e
Esha Datta <eshadatta16@gmail.com>
2016-02-11 15:23:00 -0500

    DRY attempt 1

--- config/initializers/get_md_fields.rb
+++ config/initializers/get_md_fields.rb
@@ -1,1 +1,1 @@
-MD_FIELDS = YAML.load_file(File.join(Rails.root, "config", "metadata_fields.yml"))["terms"].deep_symbolize_keys
+MD_FIELDS = YAML.load_file(File.join(Rails.root, "config", "metadata_fields.yml"))["terms"]["vocabulary"].deep_symbolize_keys
