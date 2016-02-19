ba40561bedc4210e1dc13def74d5e71ef4df969c
Esha Datta <eshadatta16@gmail.com>
2016-02-19 09:55:02 -0500

    changed routine names and separated out default values for namespace and occurrence

--- app/models/concerns/metadata_fields.rb
+++ app/models/concerns/metadata_fields.rb
@@ -2,9 +2,12 @@ module MetadataFields
 	extend ActiveSupport::Concern
 	extend self
    
-   DEFAULT_VALUE = 'all' 
+   NAMESPACE_ALL = 'all'
+   MULTIPLE_ALL  = 'all'
+   DEFAULT_NAMESPACE = NAMESPACE_ALL
+   DEFAULT_MULTIPLE  = MULTIPLE_ALL
 
-   def process_metadata_fields(ns:DEFAULT_VALUE, multiple: DEFAULT_VALUE)
+   def process_metadata_fields(ns:DEFAULT_NAMESPACE, multiple: DEFAULT_MULTIPLE)
    	  unless allowed_values(ns,multiple)
    	  	raise ArgumentError.new(
           "#{multiple} should be one of these values: #{allowed_values_for_multiple} 
@@ -14,29 +17,35 @@ module MetadataFields
    	  chk_key = ns.to_sym
       fields = []
    	  if METADATA_FIELDS.has_key?(chk_key)
-   	  	process_source_fields(chk_key,multiple,fields)
-   	  elsif ns == DEFAULT_VALUE 
-   	  	process_all_fields(multiple,fields)
+   	  	add_fields_by_ns(multiple,fields,chk_key)
+   	  elsif ns == DEFAULT_NAMESPACE
+   	  	add_all_fields(multiple,fields)
       end
    	  fields
    end
    
-   def process_all_fields(multiple,fields)
+   # output all fields regardless of namespace
+   def add_all_fields(multiple,fields)
      METADATA_FIELDS.keys.each { |ns|
-       process_source_fields(ns,multiple,fields) 
+       add_fields_by_ns(multiple,fields,ns) 
      }
    end
 
-   def process_source_fields(ns,multiple,fields)
+   # add fields specified by namespace
+   def add_fields_by_ns(multiple,fields,ns)
      METADATA_FIELDS[ns].keys.each{ |field|
        # if output is true then print out field
-       output = multiple == DEFAULT_VALUE ? true : METADATA_FIELDS[ns][field][:multiple] == multiple
+       # if multiple has the default value
+       # or one of the values in the yml file
+       output = ( multiple == DEFAULT_MULTIPLE ) ? 
+                true : 
+                METADATA_FIELDS[ns][field][:multiple] == multiple
 	     fields.push(field) if output
      }
    end
 
    def allowed_values_for_multiple
-   	 [true, false, DEFAULT_VALUE]
+   	 [true, false, DEFAULT_MULTIPLE]
    end
 
    def allowed_values_for_ns
@@ -44,12 +53,13 @@ module MetadataFields
      METADATA_FIELDS.keys.each { |k| 
       values.push(k.to_s)
      }
-     values << DEFAULT_VALUE
+     values << DEFAULT_NAMESPACE
    end
 
    def allowed_values(ns,multiple)
       allowed_values_for_ns.include?(ns) && allowed_values_for_multiple.include?(multiple)
    end
-   private_class_method :process_all_fields, :process_source_fields, :allowed_values_for_multiple, :allowed_values_for_ns, :allowed_values
+
+   private_class_method :add_all_fields, :add_fields_by_ns, :allowed_values_for_multiple, :allowed_values_for_ns, :allowed_values
 end
 
