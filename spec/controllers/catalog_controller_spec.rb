0f4ad749f8bec30b8a7c64aacb0f1192566ad6d8
David <da70@nyu.edu>
2016-02-22 23:46:15 -0500

    catalog_controller_spec: new methods for returning expected ordering of fields for facet, index, and show fields

--- spec/controllers/catalog_controller_spec.rb
+++ spec/controllers/catalog_controller_spec.rb
@@ -73,6 +73,68 @@ describe CatalogController do
   def blacklight_configuration
     @controller.instance_variable_get('@blacklight_config')
   end
+
+  def expected_facet_fields_order
+    [
+        'desc_metadata__type_sim',
+        'desc_metadata__creator_sim',
+        'desc_metadata__subject_sim',
+        'desc_metadata__language_sim',
+        'collection_sim'
+    ]
+  end
+
+  def expected_index_fields_order
+    [
+        'desc_metadata__title_tesim',
+        'desc_metadata__title_vern_tesim',
+        'desc_metadata__author_tesim',
+        'desc_metadata__author_vern_tesim',
+        'desc_metadata__format_ssim',
+        'desc_metadata__language_tesim',
+        'desc_metadata__published_tesim',
+        'desc_metadata__published_vern_tesim',
+        'desc_metadata__lc_callnum_tesim',
+        'desc_metadata__publisher_tesim',
+        'desc_metadata__restrictions_tesim',
+        'desc_metadata__available_tesim',
+        'desc_metadata__id_tesim'
+    ]
+  end
+
+  def expected_show_fields_order
+    [
+        'desc_metadata__title_tesim',
+        'desc_metadata__creator_tesim',
+        'desc_metadata__title_vern_tesim',
+        'desc_metadata__subtitle_tesim',
+        'desc_metadata__subtitle_vern_tesim',
+        'desc_metadata__author_tesim',
+        'desc_metadata__author_vern_tesim',
+        'desc_metadata__format_ssim',
+        'desc_metadata__url_fulltext_tsim_tesim',
+        'desc_metadata__url_suppl_tsim_tesim',
+        'desc_metadata__language_tesim',
+        'desc_metadata__published_tesim',
+        'desc_metadata__published_vern_tesim',
+        'desc_metadata__lc_callnum_tesim',
+        'desc_metadata__isbn_tesim',
+        'desc_metadata__publisher_tesim',
+        'desc_metadata__type_tesim',
+        'desc_metadata__description_tesim',
+        'desc_metadata__series_tesim',
+        'desc_metadata__version_tesim',
+        'desc_metadata__restrictions_tesim',
+        'desc_metadata__available_tesim',
+        'desc_metadata__relation_tesim',
+        'desc_metadata__location_tesim',
+        'desc_metadata__repo_tesim',
+        'desc_metadata__data_provider_tesim',
+        'desc_metadata__addinfolink_tesim',
+        'desc_metadata__rights_tesim'
+    ]
+  end
+
   def response_qf
     assigns_response["responseHeader"]["params"]["qf"]
   end
