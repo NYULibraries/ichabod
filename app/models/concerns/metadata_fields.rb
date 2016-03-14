840b14ebb1a2a6389b5259dbe06fb438a036fd64
Esha Datta <eshadatta16@gmail.com>
2016-03-14 15:09:58 -0400

    adding methods to grab uri and namespace

--- app/models/concerns/metadata_fields.rb
+++ app/models/concerns/metadata_fields.rb
@@ -7,6 +7,7 @@ module MetadataFields
    DEFAULT_NAMESPACE = NAMESPACE_ALL
    DEFAULT_MULTIPLE  = MULTIPLE_ALL
 
+
    def process_metadata_fields(ns:DEFAULT_NAMESPACE, multiple: DEFAULT_MULTIPLE)
    	  unless allowed_values(ns,multiple)
    	  	raise ArgumentError.new(
@@ -24,6 +25,33 @@ module MetadataFields
    	  fields
    end
    
+   def get_source_info(ns:DEFAULT_NAMESPACE)
+     unless allowed_values_for_ns(ns)
+      raise ArgumentError.new("#{ns} should be one of these values: #{allowed_values_for_ns}")   
+     end
+     chk_key = ns.to_sym
+     if METADATA_FIELDS.has_key?(chk_key)
+      get_metadata_source_info(chk_key)
+     elsif ns == DEFAULT_NAMESPACE
+      get_all_sources_info
+     end
+       
+   end
+    
+   def get_all_sources_info
+    source_info = {}
+    METADATA_FIELDS.keys.each { |ns|
+      source_info[ns] = get_metadata_source_info(ns)
+    }
+    source_info
+   end
+
+   def get_metadata_source_info(ns)
+     ns = METADATA_FIELDS[ns][:info][:namespace]
+     uri = METADATA_FIELDS[ns][:info][:uri]
+     info = { ns: ns, uri: uri }
+   end
+   
    # output all fields regardless of namespace
    def add_all_fields(multiple,fields)
      METADATA_FIELDS.keys.each { |ns|
@@ -33,13 +61,15 @@ module MetadataFields
 
    # add fields specified by namespace
    def add_fields_by_ns(multiple,fields,ns)
-     METADATA_FIELDS[ns].keys.each{ |field|
+     #grabbing only fields
+     metadata_fields = METADATA_FIELDS[ns][:fields]
+     METADATA_FIELDS[ns][:fields].keys.each{ |field|
        # if output is true then print out field
        # if multiple has the default value
        # or one of the values in the yml file
        output = ( multiple == DEFAULT_MULTIPLE ) ? 
                 true : 
-                METADATA_FIELDS[ns][field][:multiple] == multiple
+                metadata_fields[field][:multiple] == multiple
 	     fields.push(field) if output
      }
    end
@@ -60,6 +90,7 @@ module MetadataFields
       allowed_values_for_ns.include?(ns) && allowed_values_for_multiple.include?(multiple)
    end
 
-   private_class_method :add_all_fields, :add_fields_by_ns, :allowed_values_for_multiple, :allowed_values_for_ns, :allowed_values
+   private_class_method :add_all_fields, :add_fields_by_ns, :allowed_values_for_multiple, :allowed_values_for_ns, :allowed_values,
+                        :get_all_sources_info, :get_metadata_source_info
 end
 
--- config/initializers/get_metadata_fields.rb
+++ config/initializers/get_metadata_fields.rb
@@ -1,3 +1,3 @@
 #METADATA_FIELDS = YAML.load_file(File.join(Rails.root, "config", "metadata_fields.yml"))["terms"]["vocabulary"].deep_symbolize_keys
 METADATA_FIELDS = YAML.load_file(File.join(Rails.root, "config", "metadata_fields_ns.yml"))["terms"]["vocabulary"].deep_symbolize_keys
-binding.pry
+
