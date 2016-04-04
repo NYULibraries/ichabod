7fcf0ac3a5c871b32441e607734d4917b3ddb55b
Esha Datta <eshadatta16@gmail.com>
2016-04-04 11:09:19 -0400

    changed variable checks to *all in module and adding tests to rspec file

--- app/models/concerns/metadata_fields.rb
+++ app/models/concerns/metadata_fields.rb
@@ -1,101 +1,96 @@
 module MetadataFields
 	extend ActiveSupport::Concern
 	extend self
-   
-   NAMESPACE_ALL = 'all'
-   MULTIPLE_ALL  = 'all'
-   DEFAULT_NAMESPACE = NAMESPACE_ALL
-   DEFAULT_MULTIPLE  = MULTIPLE_ALL
 
-   # get metadata fields
-   def process_metadata_fields(ns:DEFAULT_NAMESPACE, multiple: DEFAULT_MULTIPLE)
-   	  unless allowed_values(ns,multiple)
-   	  	raise ArgumentError.new(
-          "#{multiple} should be one of these values: #{allowed_values_for_multiple} 
-          or #{ns} should be one of these values: #{allowed_values_for_ns}")	
-   	  end
+	NAMESPACE_ALL = 'all'
+	MULTIPLE_ALL  = 'all'
+	DEFAULT_NAMESPACE = NAMESPACE_ALL
+	DEFAULT_MULTIPLE  = MULTIPLE_ALL
 
-   	  chk_key = ns.to_sym
-      fields = []
-   	  if METADATA_FIELDS.has_key?(chk_key)
-   	  	add_fields_by_ns(multiple,fields,chk_key)
-   	  elsif ns == DEFAULT_NAMESPACE
-   	  	add_all_fields(multiple,fields)
-      end
-   	  fields
-   end
-   
-   # get namespace and uri for
-   # metadata namespace
-   def get_source_info(ns:DEFAULT_NAMESPACE)
-     unless allowed_values_for_ns.include?(ns)
-      raise ArgumentError.new("#{ns} should be one of these values: #{allowed_values_for_ns}")   
-     end
-     chk_key = ns.to_sym
-     if METADATA_FIELDS.has_key?(chk_key)
-      get_metadata_source_info(chk_key)
-     elsif ns == DEFAULT_NAMESPACE
-      get_all_sources_info
-     end
-       
-   end
-    
-   # get namespace and uri for all 
-   # metadata sources 
-   def get_all_sources_info
-    source_info = {}
-    METADATA_FIELDS.keys.each { |ns|
-      source_info[ns] = get_metadata_source_info(ns)
-    }
-    source_info
-   end
+	# get metadata fields
+	def process_metadata_fields(ns:DEFAULT_NAMESPACE, multiple: DEFAULT_MULTIPLE)
+		unless allowed_values_for_ns.include?(ns)
+			raise ArgumentError.new("#{ns} should be one of these values: #{allowed_values_for_ns}")
+		end
+		unless allowed_values_for_multiple.include?(multiple)
+			raise ArgumentError.new("#{multiple} should be one of these values: #{allowed_values_for_multiple}")
+		end
+		chk_key = ns.to_sym
+		fields = []
+		if METADATA_FIELDS.has_key?(chk_key)
+			add_fields_by_ns(multiple,fields,chk_key)
+		elsif ns == NAMESPACE_ALL
+			add_all_fields(multiple,fields)
+		end
+		fields
+	end
 
-   # get source info by namespace
-   def get_metadata_source_info(ns)
-     namespace = METADATA_FIELDS[ns][:info][:namespace]
-     uri = METADATA_FIELDS[ns][:info][:uri]
-     info = { namespace: namespace, uri: uri }
-   end
-   
-   # output all fields regardless of namespace
-   def add_all_fields(multiple,fields)
-     METADATA_FIELDS.keys.each { |ns|
-       add_fields_by_ns(multiple,fields,ns) 
-     }
-   end
+	# get namespace and uri for
+	# metadata namespace
+	def get_source_info(ns:DEFAULT_NAMESPACE)
+		unless allowed_values_for_ns.include?(ns)
+			raise ArgumentError.new("#{ns} should be one of these values: #{allowed_values_for_ns}")
+		end
+		chk_key = ns.to_sym
+		if METADATA_FIELDS.has_key?(chk_key)
+			get_metadata_source_info(chk_key)
+		elsif ns == NAMESPACE_ALL
+			get_all_sources_info
+		end
 
-   # add fields specified by namespace
-   def add_fields_by_ns(multiple,fields,ns)
-     #grabbing only fields
-     metadata_fields = METADATA_FIELDS[ns][:fields]
-     METADATA_FIELDS[ns][:fields].keys.each{ |field|
-       # if output is true then print out field
-       # if multiple has the default value
-       # or one of the values in the yml file
-       output = ( multiple == DEFAULT_MULTIPLE ) ? 
-                true : 
-                metadata_fields[field][:multiple] == multiple
-	     fields.push(field) if output
-     }
-   end
+	end
 
-   def allowed_values_for_multiple
-   	 [true, false, DEFAULT_MULTIPLE]
-   end
+	# get namespace and uri for all
+	# metadata sources
+	def get_all_sources_info
+		source_info = {}
+		METADATA_FIELDS.keys.each { |ns|
+			source_info[ns] = get_metadata_source_info(ns)
+		}
+		source_info
+	end
 
-   def allowed_values_for_ns
-    values = []
-     METADATA_FIELDS.keys.each { |k| 
-      values.push(k.to_s)
-     }
-     values << DEFAULT_NAMESPACE
-   end
+	# get source info by namespace
+	def get_metadata_source_info(ns)
+		namespace = METADATA_FIELDS[ns][:info][:namespace]
+		uri = METADATA_FIELDS[ns][:info][:uri]
+		info = { namespace: namespace, uri: uri }
+	end
 
-   def allowed_values(ns,multiple)
-      allowed_values_for_ns.include?(ns) && allowed_values_for_multiple.include?(multiple)
-   end
+	# output all fields regardless of namespace
+	def add_all_fields(multiple,fields)
+		METADATA_FIELDS.keys.each { |ns|
+			add_fields_by_ns(multiple,fields,ns)
+		}
+	end
 
-   private_class_method :add_all_fields, :add_fields_by_ns, :allowed_values_for_multiple, :allowed_values_for_ns, :allowed_values,
-                        :get_all_sources_info, :get_metadata_source_info
+	# add fields specified by namespace
+	def add_fields_by_ns(multiple,fields,ns)
+		#grabbing only fields
+		metadata_fields = METADATA_FIELDS[ns][:fields]
+		METADATA_FIELDS[ns][:fields].keys.each{ |field|
+			# if output is true then print out field
+			# if multiple has the default value
+			# or one of the values in the yml file
+			output = ( multiple == DEFAULT_MULTIPLE ) ?
+			true :
+			metadata_fields[field][:multiple] == multiple
+			fields.push(field) if output
+		}
+	end
+
+	def allowed_values_for_multiple
+		[true, false, MULTIPLE_ALL]
+	end
+
+	def allowed_values_for_ns
+		values = []
+		METADATA_FIELDS.keys.each { |k|
+			values << k.to_s
+		}
+		values << NAMESPACE_ALL
+	end
+
+	private_class_method :add_all_fields, :add_fields_by_ns, :allowed_values_for_multiple, :allowed_values_for_ns,
+	:get_all_sources_info, :get_metadata_source_info
 end
-
--- spec/models/concerns/metadata_fields_spec.rb
+++ spec/models/concerns/metadata_fields_spec.rb
@@ -1,58 +1,120 @@
 require 'spec_helper'
 
+def read_yaml_file
+	YAML.load_file(File.join(Rails.root, "config", "metadata_fields.yml"))["terms"]["vocabulary"].deep_symbolize_keys
+end
+
+def get_fields_all_sources
+	contents = read_yaml_file
+	all_fields = []
+	contents.keys.each { |ns|
+		contents[ns][:fields].keys.each { |f|
+			all_fields << f
+		}
+	}
+	all_fields
+end
+
+def get_fields_by_source(source, multiple = nil)
+	fields = []
+	contents = read_yaml_file
+	metadata_fields = contents[source][:fields]
+	contents[source][:fields].keys.each{ |field|
+		output = multiple.nil? ?
+		true :
+		metadata_fields[field][:multiple] == multiple
+		fields << field if output
+	}
+	fields
+end
+
+def get_all_sources_info
+
+end
+def get_source_info_by_source(ns)
+
+end
+
+
 describe MetadataFields do
 	let(:invalid_value) { 'foo' }
-	let(:source) { 'nyucore' }
 	let(:all) { MetadataFields.process_metadata_fields }
-	let(:single_source) { MetadataFields.process_metadata_fields(ns:source, multiple: false) }
 	let(:single_source_info) { MetadataFields.get_source_info(ns:source) }
 	let(:all_sources_info) { MetadataFields.get_source_info }
 	let(:dummy_class) { Class.new { include MetadataFields } }
 
-	
+
 	describe 'process_metadata_fields' do
 		it { MetadataFields.should respond_to :process_metadata_fields }
 
-		it 'raises an error if an invalid namespace value is passed to the method' do 
-		    expect { MetadataFields.process_metadata_fields(ns: invalid_value) }.to raise_error
+		it 'raises an error if an invalid namespace value is passed to the method' do
+			expect { MetadataFields.process_metadata_fields(ns: invalid_value) }.to raise_error
 		end
 
-		it 'raises an error if an invalid occurrence value is passed to the method' do 
-		    expect { MetadataFields.process_metadata_fields(multiple: invalid_value) }.to raise_error
+		it 'raises an error if an invalid occurrence value is passed to the method' do
+			expect { MetadataFields.process_metadata_fields(multiple: invalid_value) }.to raise_error
 		end
 
-		it 'returns an array if no arguments are sent' do 
-			expect(all).to be_a_kind_of(Array)
+
+		context 'check method returns based on various valid parameters' do
+			sources = read_yaml_file
+			it 'returns an array if no arguments are sent' do
+				expect(all).to be_a_kind_of(Array)
+			end
+
+			it 'should equal all fields in the yml file if no arguments are sent' do
+				all_fields = get_fields_all_sources
+				expect(all).to match_array(all_fields)
+			end
+
+			sources.keys.each { |ns|
+				ns = ns.to_s
+				it "should return fields for a specific sources: #{ns} and no occurrences specified" do
+					fields = get_fields_by_source(ns)
+					fields_from_module = MetadataFields.process_metadata_fields(ns:ns)
+					expect(fields_from_module).to match_array(fields)
+				end
+
+				it "should return fields for a specific source: #{ns} and for multiple occurrences" do
+					multiple = true
+					fields = get_fields_by_source(ns,multiple:multiple)
+					fields_from_module = MetadataFields.process_metadata_fields(ns:ns,multiple:multiple)
+					expect(fields_from_module).to match_array(fields)
+				end
+
+				it "should return fields for a specific source: #{ns} and for single occurrences" do
+					multiple = false
+					fields = get_fields_by_source(ns,multiple:multiple)
+					fields_from_module = MetadataFields.process_metadata_fields(ns:ns,multiple:multiple)
+					expect(fields_from_module).to match_array(fields)
+				end
+			}
 		end
 
-		it 'returns an array, if passing valid values for namespace and occurrence' do	    
-		    expect(single_source).to be_an(Array)
+		context 'use of process_metadata_fields in an instance of a class' do
+			subject { dummy_class.new }
+			it { should respond_to(:process_metadata_fields) }
 		end
-  	
-  	    context 'use of process_metadata_fields in an instance of a class' do
-  	       subject { dummy_class.new }
-  	       it { should respond_to(:process_metadata_fields) }
-  	    end
-	end	
+	end
 
 	describe 'get_source_info' do
-	    it { MetadataFields.should respond_to :get_source_info }
+		it { MetadataFields.should respond_to :get_source_info }
 
-		it 'raises an error if an invalid namespace value is passed to the method' do 
-		    expect { MetadataFields.get_source_info(ns: invalid_value) }.to raise_error
+		it 'raises an error if an invalid namespace value is passed to the method' do
+			expect { MetadataFields.get_source_info(ns: invalid_value) }.to raise_error
 		end
 
-		it 'returns a hash if no arguments are sent' do 
+		it 'returns a hash if no arguments are sent' do
 			expect(all_sources_info).to be_a_kind_of(Hash)
 		end
 
-		it 'returns a hash, if passing a valid value for namespace' do	    
-		    expect(single_source_info).to be_an(Hash)
+		it 'returns a hash, if passing a valid value for namespace' do
+			expect(single_source_info).to be_an(Hash)
 		end
-  	
-  	    context 'use of process_metadata_fields in an instance of a class' do
-  	       subject { dummy_class.new }
-  	       it { should respond_to(:get_source_info) }
-  	    end
-  	end
-end 
+
+		context 'use of process_metadata_fields in an instance of a class' do
+			subject { dummy_class.new }
+			it { should respond_to(:get_source_info) }
+		end
+	end
+end
