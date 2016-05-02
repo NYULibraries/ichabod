ab50013314167cff83424758a9d503507288ecaf
David <da70@nyu.edu>
2016-05-02 12:30:16 -0400

    MetadataFields renames: change *fields* to *field_names*

--- app/models/concerns/metadata_fields.rb
+++ app/models/concerns/metadata_fields.rb
@@ -8,7 +8,7 @@ module MetadataFields
 	DEFAULT_MULTIPLE  = MULTIPLE_ALL
 	METADATA_FIELDS = YAML.load_file(File.join(Rails.root, "config", "metadata_fields.yml"))["terms"]["vocabulary"].deep_symbolize_keys
 	# get metadata fields
-	def process_metadata_fields(ns:DEFAULT_NAMESPACE, multiple: DEFAULT_MULTIPLE)
+	def process_metadata_field_names(ns:DEFAULT_NAMESPACE, multiple: DEFAULT_MULTIPLE)
 		unless allowed_values_for_ns.include?(ns)
 			raise ArgumentError.new("#{ns} should be one of these values: #{allowed_values_for_ns}")
 		end
@@ -16,13 +16,13 @@ module MetadataFields
 			raise ArgumentError.new("#{multiple} should be one of these values: #{allowed_values_for_multiple}")
 		end
 		chk_key = ns.to_sym
-		fields = []
+		field_names = []
 		if METADATA_FIELDS.has_key?(chk_key)
-			add_fields_by_ns(multiple,fields,chk_key)
+			add_field_names_by_ns(multiple, field_names, chk_key)
 		elsif ns == NAMESPACE_ALL
-			add_all_fields(multiple,fields)
+			add_all_field_names(multiple, field_names)
 		end
-		fields
+		field_names
 	end
 
 	# get namespace and uri for
@@ -56,23 +56,23 @@ module MetadataFields
 	end
 
 	# output all fields regardless of namespace
-	def add_all_fields(multiple,fields)
+	def add_all_field_names(multiple, fields)
 		METADATA_FIELDS.keys.each { |ns|
-			add_fields_by_ns(multiple,fields,ns)
+			add_field_names_by_ns(multiple, fields, ns)
 		}
 	end
 
 	# add fields specified by namespace
-	def add_fields_by_ns(multiple,fields,ns)
+	def add_field_names_by_ns(multiple, fields, ns)
 		#grabbing only fields
-		metadata_fields = METADATA_FIELDS[ns][:fields]
+		metadata_field_names = METADATA_FIELDS[ns][:fields]
 		METADATA_FIELDS[ns][:fields].keys.each{ |field|
 			# if output is true then print out field
 			# if multiple has the default value
 			# or one of the values in the yml file
 			output = ( multiple == DEFAULT_MULTIPLE ) ?
 			true :
-			metadata_fields[field][:multiple] == multiple
+			metadata_field_names[field][:multiple] == multiple
 			fields.push(field) if output
 		}
 	end
@@ -89,6 +89,6 @@ module MetadataFields
 		values << NAMESPACE_ALL
 	end
 
-	private_class_method :add_all_fields, :add_fields_by_ns, :allowed_values_for_multiple, :allowed_values_for_ns,
-	:get_all_sources_info, :get_metadata_source_info
+	private_class_method :add_all_field_names, :add_field_names_by_ns, :allowed_values_for_multiple, :allowed_values_for_ns,
+											 :get_all_sources_info, :get_metadata_source_info
 end
