e113b44750190ea14560765c9175b037c9417aa9
Corey Harper <corey.harper@gmail.com>
2016-08-09 15:51:54 -0400

    changing whitespace in qf to match development

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -15,7 +15,7 @@ class CatalogController < ApplicationController
     config.default_solr_params = {
       :qf => 'desc_metadata__title_tesim desc_metadata__author_tesim desc_metadata__publisher_tesim
                 desc_metadata__type_tesim desc_metadata__description_tesim desc_metadata__series_tesim
-                desc_metadata__creator_tesim desc_metadata__subject_tesim desc_metadata__subject_spatial_tesim
+                desc_metadata__creator_tesim desc_metadata__subject_tesim desc_metadata__subject_spatial_tesim 
                 desc_metadata__subject_temporal_tesim desc_metadata__isbn_tesim desc_metadata__genre_tesim',
       :qt => 'search',
       :rows => 10
