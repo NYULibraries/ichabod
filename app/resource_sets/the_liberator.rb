499b3d1b0aadedb95ccc6629cbef875cb8441f68
David <da70@nyu.edu>
2016-08-29 15:04:53 -0400

    The Liberator resource set: initial commit

---
+++ app/resource_sets/the_liberator.rb
@@ -0,0 +1,15 @@
+class TheLiberator < Ichabod::ResourceSet::Base
+  self.prefix = 'theliberator'
+  self.collection_title = "The Liberator"
+  self.source_reader = :the_liberator
+
+  attr_reader :endpoint_url, :start, :rows
+  alias_method :collection_code, :prefix
+
+  def initialize(*args)
+    @endpoint_url = args.shift
+    @start = args.shift || '0'
+    @rows = args.shift || '77'
+    super
+  end
+end
