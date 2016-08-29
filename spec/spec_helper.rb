54c9db4fe562ff39987ec2a64e757f02a0a67714
David <da70@nyu.edu>
2016-08-29 15:06:50 -0400

    spec_helper.rb: add data load for The Liberator

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -47,6 +47,7 @@ if Rails.env.test?
     Ichabod::DataLoader.new('lib_guides', File.join(Rails.root, 'ingest/test_libguides.xml')).load
     Ichabod::DataLoader.new('faculty_digital_archive_ngo',File.join(Rails.root, 'ingest/test_ngo_fda.csv')).load
     Ichabod::DataLoader.new('nyu_press_open_access_book', 'http://discovery.dlib.nyu.edu:8080/solr3_discovery/nyupress/select','0','5').load
+    Ichabod::DataLoader.new('the_liberator', 'http://discovery.dlib.nyu.edu:8080/solr3_discovery/viewer/select','0','5').load
     Ichabod::DataLoader.new('faculty_digital_archive_service_data', File.join(Rails.root, 'ingest/test_data_service.csv')).load
     Ichabod::DataLoader.new('masses','http://dlib.nyu.edu/themasses/books.json','0','5').load
     Ichabod::DataLoader.new('indian_ocean_data', File.join(Rails.root, 'ingest/test_io.csv')).load
