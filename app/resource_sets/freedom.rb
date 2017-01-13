6c3927b47504a5ca27c1fed719bc745146847961
David <da70@nyu.edu>
2017-01-13 16:00:23 -0500

    app/resource_sets/freedom.rb: initial commit

---
+++ app/resource_sets/freedom.rb
@@ -0,0 +1,15 @@
+class Freedom < Ichabod::ResourceSet::Base
+  self.prefix = 'freedom'
+  self.collection_title = "Freedom"
+  self.source_reader = :freedom_reader
+
+  attr_reader :endpoint_url, :start, :rows
+  alias_method :collection_code, :prefix
+
+  def initialize(*args)
+    @endpoint_url = args.shift
+    @start = args.shift || '0'
+    @rows = args.shift || '45'
+    super
+  end
+end
