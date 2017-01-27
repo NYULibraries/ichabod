37b1c52f079944bff4eb7a16868ac88d1a28327d
David <da70@nyu.edu>
2017-01-27 15:03:45 -0500

    Freedom resource set: switch to new TamimentJournalReader

--- app/resource_sets/freedom.rb
+++ app/resource_sets/freedom.rb
@@ -1,13 +1,14 @@
 class Freedom < Ichabod::ResourceSet::Base
   self.prefix = 'fdm'
   self.collection_title = "Freedom"
-  self.source_reader = :freedom_reader
+  self.source_reader = :tamiment_journal_reader
 
-  attr_reader :endpoint_url, :start, :rows
+  attr_reader :endpoint_url, :journal, :start, :rows
   alias_method :collection_code, :prefix
 
   def initialize(*args)
     @endpoint_url = args.shift
+    @journal = self.collection_title
     @start = args.shift || '0'
     @rows = args.shift || '45'
     super
